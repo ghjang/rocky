6703d7d48a93475257fd8bed54240885a7b8f34b
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-11-13 01:32:04 +0900

    made the integral constant type name shorter.
    - made to use the parameter type’s ’type’ member as the result type if
    any.

--- rocky/TypeSelection.h
+++ rocky/TypeSelection.h
@@ -4,20 +4,29 @@
 
 #include <type_traits>
 
+#include "rocky/HasTypeMember.h"
+
+
+template <typename T>
+T SelectTypeIfImpl(...);
+
+template <typename T, typename = std::enable_if_t<HasTypeMember<T>::value>>
+typename T::type SelectTypeIfImpl(int);
+
 
 template <typename boolConstant, typename T1, typename T2>
 struct SelectTypeIf;
 
 template <typename T1, typename T2>
-struct SelectTypeIf<std::integral_constant<bool, true>, T1, T2>
+struct SelectTypeIf<std::true_type, T1, T2>
 {
-    using type = T1;
+    using type = decltype(SelectTypeIfImpl<T1>(int{}));
 };
 
 template <typename T1, typename T2>
-struct SelectTypeIf<std::integral_constant<bool, false>, T1, T2>
+struct SelectTypeIf<std::false_type, T1, T2>
 {
-    using type = T2;
+    using type = decltype(SelectTypeIfImpl<T2>(int{}));
 };
 
 
