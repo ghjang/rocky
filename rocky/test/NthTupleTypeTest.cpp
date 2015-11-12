8961fdcb95712349e4dbff73aca6f3ee51721400
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-11-13 01:34:39 +0900

    updated test cases.

--- rocky/test/NthTupleTypeTest.cpp
+++ rocky/test/NthTupleTypeTest.cpp
@@ -18,5 +18,22 @@ TEST_CASE("N-th tuple element type", "[tuple]")
     //static_assert(is_same<NthTupleElementType<t, -1>::type, double>::value, "this is a static assertion failure.");
     //static_assert(is_same<NthTupleElementType<t, 5>::type, double>::value, "this is a static assertion failure.");
 
+    struct A { };
+    struct B { int i; };
+    struct C { using type = int; };
+    struct D { void f() { } };
+    using t1 = tuple<char, A, short, B, int, C, float, D, double>;
+    static_assert(is_same<NthTupleElementType<t1, 0>::type, char>::value, "t1's 0-th should be char.");
+    static_assert(is_same<NthTupleElementType<t1, 1>::type, A>::value, "t1's 1-th should be struct A.");
+    static_assert(is_same<NthTupleElementType<t1, 2>::type, short>::value, "t1's 2-th should be char.");
+    static_assert(is_same<NthTupleElementType<t1, 3>::type, B>::value, "t1's 3-th should be struct B.");
+    static_assert(is_same<NthTupleElementType<t1, 4>::type, int>::value, "t1's 4-th should be char.");
+    static_assert(is_same<NthTupleElementType<t1, 5>::type, C>::value, "t1's 5-th should be struct C.");
+    static_assert(is_same<NthTupleElementType<t1, 6>::type, float>::value, "t1's 6-th should be char.");
+    static_assert(is_same<NthTupleElementType<t1, 7>::type, D>::value, "t1's 7-th should be struct D.");
+    static_assert(is_same<NthTupleElementType<t1, 8>::type, double>::value, "t1's 8-th should be char.");
+
+    //static_assert(is_same<NthTupleElementType<t1, -1>::type, double>::value, "this is a static assertion failure.");
+    //static_assert(is_same<NthTupleElementType<t1, 9>::type, double>::value, "this is a static assertion failure.");
 }
 
