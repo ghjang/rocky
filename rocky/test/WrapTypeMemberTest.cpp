57bea155ff237b72b90d1173e82931969e0b5d9b
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-11-13 01:35:07 +0900

    added WrapTypeMember test.

---
+++ rocky/test/WrapTypeMemberTest.cpp
@@ -0,0 +1,26 @@
+#include "catch.hpp"
+
+#include "rocky/WrapTypeMember.h"
+
+#include <type_traits>
+
+#include "rocky/HasTypeMember.h"
+
+
+TEST_CASE("Wrap as type member", "[WrapTypeMember]")
+{
+    using std::is_same;
+
+    struct A { };
+    struct B { using type = int; };
+
+    using WrappedA = WrapAsTypeMember<A>;
+    static_assert(HasTypeMember<WrappedA>::value, "WrappedA should have 'type' member.");
+    static_assert(is_same<WrappedA::type, A>::value, "WrappedA::type should be A.");
+
+    using WrappedB = WrapAsTypeMember<B>;
+    static_assert(HasTypeMember<WrappedB>::value, "WrappedB should have 'type' member.");
+    static_assert(is_same<WrappedB::type, B>::value, "WrappedB::type should be B.");
+    static_assert(is_same<WrappedB::type::type, int>::value, "WrappedB::type::type should be int.");
+}
+
