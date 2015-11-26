66d441d9983ef50238bb120cab6e1d5ce4cdfe64
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-11-26 18:51:54 +0900

    added SumOfTupleElementTypeSize metafiction.

--- rocky/meta/TupleSize.h
+++ rocky/meta/TupleSize.h
@@ -2,5 +2,25 @@
 #define ROCKY_TUPLESIZE_H
 
 
+#include <type_traits>
+#include <tuple>
+
+#include "rocky/meta/FoldRight.h"
+#include "rocky/meta/IntegralConstantUtility.h"
+
+
+template <typename Tuple>
+struct SumOfTupleElementTypeSize;
+
+template <typename... list>
+struct SumOfTupleElementTypeSize<std::tuple<list...>>
+            : FoldRight<
+                    IntegralConstantSum,
+                    std::integral_constant<std::size_t, 0>,
+                    std::integral_constant<std::size_t, sizeof(list)>...
+                >::type
+{ };
+
 
 #endif //ROCKY_TUPLESIZE_H
+
--- rocky/test/TupleSizeTest.cpp
+++ rocky/test/TupleSizeTest.cpp
@@ -5,6 +5,22 @@
 
 TEST_CASE("Getting the size of tuple's template parameters", "[TupleSize]")
 {
+    using std::tuple;
 
+    static_assert(
+            SumOfTupleElementTypeSize<tuple<int, int, int>>() == sizeof(int) * 3,
+            "element type size of tuple<int, int, int> == sizeof(int) * 3"
+    );
+
+    struct S
+    {
+        int i_;
+        char c_;
+    };
+
+    static_assert(
+            SumOfTupleElementTypeSize<tuple<int, S, double>>() == sizeof(int) + sizeof(S) + sizeof(double),
+            "element type size of tuple<int, S, double> == sizeof(int) + sizeof(S) + sizeof(double)"
+    );
 }
 
