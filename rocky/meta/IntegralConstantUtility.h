088ed2f6862892b42e82666de84ff0d6f4525a68
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-11-27 23:04:43 +0900

    added TypeToBoolIntegralConstant class template.

--- rocky/meta/IntegralConstantUtility.h
+++ rocky/meta/IntegralConstantUtility.h
@@ -5,6 +5,7 @@
 #include <type_traits>
 
 #include "rocky/StringNumberConversion.h"
+#include "rocky/meta/TypeSelection.h"
 
 
 template <typename T1, T1 v1, typename T2, T2 v2>
@@ -35,6 +36,10 @@ template <int i>
 constexpr std::integral_constant<int, i> int_c{};
 
 
+using zero_t = std::integral_constant<int, 0>;
+using one_t = std::integral_constant<int, 1>;
+
+
 template <char... s>
 constexpr auto operator ""_c ()
 {
@@ -61,6 +66,20 @@ template <typename T1, T1 v1, typename T2, T2 v2>
 struct IntegralConstantMultiply<std::integral_constant<T1, v1>, std::integral_constant<T2, v2>>
         : std::integral_constant<decltype(v1 * v2), v1 * v2>
 { };
+
+
+template <template <typename> class Predicate>
+struct TypeToBoolIntegralConstant
+{
+    template <typename T>
+    struct Convert
+            : SelectTypeIf<
+                    std::integral_constant<bool, Predicate<T>::value>,
+                    one_t,
+                    zero_t
+                >
+    { };
+};
 
 
 #endif //ROCKY_INTEGRALCONSTANTUTILITY_H
