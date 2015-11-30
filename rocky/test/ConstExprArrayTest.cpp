2f86b85c3ccc5fc787faa9500c9e4e061a2b9216
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-01 00:34:47 +0900

    added test for ConstExprArray class.

---
+++ rocky/test/ConstExprArrayTest.cpp
@@ -0,0 +1,29 @@
+#include "catch.hpp"
+
+#include "rocky/ConstExprArray.h"
+
+
+namespace
+{
+    template <std::size_t ArraySize>
+    constexpr auto CalculateSquare()
+    {
+        ConstExprArray<int, ArraySize> result;
+        for (int i = 0; i < result.size(); ++i) {
+            result[i] = i * i;  // NOTE: this is not allowed in C++14 std::array.
+        }
+        return result;
+    }
+}
+
+
+TEST_CASE("basic usage in constexpr function", "[ConstExprArray]")
+{
+    constexpr auto resultArray = CalculateSquare<5>();
+    REQUIRE(resultArray[0] == 0);
+    REQUIRE(resultArray[1] == 1);
+    REQUIRE(resultArray[2] == 4);
+    REQUIRE(resultArray[3] == 9);
+    REQUIRE(resultArray[4] == 16);
+}
+
