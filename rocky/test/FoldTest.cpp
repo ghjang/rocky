0945d6766f2e32f93cb3229d08f5eb2a2acd88f7
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-02 02:09:13 +0900

    added more tests.

--- rocky/test/FoldTest.cpp
+++ rocky/test/FoldTest.cpp
@@ -21,6 +21,22 @@ TEST_CASE("fold right, integral constant sum", "[Fold]")
     static_assert(sum_t() == 15, "1 + (2 + (3 + (4 + (5 + 0)))) = 15");
 }
 
+TEST_CASE("fold right, integral constant subtract", "[Fold]")
+{
+    using std::integral_constant;
+
+    using subtract_t = typename FoldRight<
+                                    IntegralConstantSubtract,
+                                    integral_constant<int, 0>,  // init
+                                    integral_constant<int, 1>,
+                                    integral_constant<int, 2>,
+                                    integral_constant<int, 3>,
+                                    integral_constant<int, 4>,
+                                    integral_constant<int, 5>
+                                >::type;
+    static_assert(subtract_t() == 3, "(1 - (2 - (3 - (4 - (5 - 0))))) = 3");
+}
+
 TEST_CASE("fold right, integral constant in tuple sum", "[Fold]")
 {
     using std::integral_constant;
@@ -72,6 +88,22 @@ TEST_CASE("fold left, integral constant sum", "[Fold]")
                                 integral_constant<int, 5>
                             >::type;
     static_assert(sum_t() == 15, "(((((0 + 1) + 2) + 3) + 4) + 5) = 15");
+}
+
+TEST_CASE("fold left, integral constant subtract", "[Fold]")
+{
+    using std::integral_constant;
+
+    using subtract_t = typename FoldLeft<
+                                    IntegralConstantSubtract,
+                                    integral_constant<int, 0>,  // init
+                                    integral_constant<int, 1>,
+                                    integral_constant<int, 2>,
+                                    integral_constant<int, 3>,
+                                    integral_constant<int, 4>,
+                                    integral_constant<int, 5>
+                                >::type;
+    static_assert(subtract_t() == -15, "(((((0 - 1) - 2) - 3) - 4) - 5) = -15");
 }
 
 TEST_CASE("fold left, integral constant in tuple sum", "[Fold]")
