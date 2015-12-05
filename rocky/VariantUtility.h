99c7c4ce2a1cdff0c14b97a72aa7cd22eb39d9b1
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-06 02:51:02 +0900

    added support for char types.
    - need to add tests more.
    - need to refactor.

--- rocky/VariantUtility.h
+++ rocky/VariantUtility.h
@@ -3,6 +3,7 @@
 
 
 #include <type_traits>
+#include <string>
 #include <vector>
 
 #include "boost/variant.hpp"
@@ -22,16 +23,83 @@ struct TupleToVariant<std::tuple<list...>>
 
 
 template <typename T>
-decltype(auto) MakeVariant(const T && arg)
+struct CharPtrTypeToStrType
+{
+    using type = T;
+};
+
+template <std::size_t N>
+struct CharPtrTypeToStrType<char [N]>
+{
+    using type = std::string;
+};
+
+template <std::size_t N>
+struct CharPtrTypeToStrType<char const (&) [N]>
+{
+    using type = std::string;
+};
+
+template <>
+struct CharPtrTypeToStrType<char const *>
+{
+    using type = std::string;
+};
+
+template <>
+struct CharPtrTypeToStrType<char *>
+{
+    using type = std::string;
+};
+
+template <std::size_t N>
+struct CharPtrTypeToStrType<wchar_t [N]>
+{
+    using type = std::wstring;
+};
+
+template <std::size_t N>
+struct CharPtrTypeToStrType<wchar_t const (&) [N]>
+{
+    using type = std::wstring;
+};
+
+template <>
+struct CharPtrTypeToStrType<wchar_t const *>
+{
+    using type = std::wstring;
+};
+
+template <>
+struct CharPtrTypeToStrType<wchar_t *>
+{
+    using type = std::wstring;
+};
+
+
+template <typename T>
+decltype(auto) MakeVariant(const T & arg)
 {
     return boost::variant<T>(arg);
 }
 
+decltype(auto) MakeVariant(const char * s)
+{
+    return boost::variant<std::string>(std::string(s));
+}
+
+decltype(auto) MakeVariant(const wchar_t * s)
+{
+    return boost::variant<std::wstring>(std::wstring(s));
+}
+
 
 template <typename... Args>
-decltype(auto) MakeVariantVector(const Args &&...  args)
+decltype(auto) MakeVariantVector(const Args &...  args)
 {
-    using unique_tuple_t = typename MakeUniqueElementTypeTuple<std::decay_t<decltype(args)>...>::type;
+    using unique_tuple_t = typename MakeUniqueElementTypeTuple<
+                                        typename CharPtrTypeToStrType<std::decay_t<decltype(args)>>::type...
+                                    >::type;
     using element_t = typename TupleToVariant<unique_tuple_t>::type;
 
     std::vector<element_t> v;
--- rocky/test/VariantUtilityTest.cpp
+++ rocky/test/VariantUtilityTest.cpp
@@ -26,16 +26,6 @@ namespace
 }
 
 
-TEST_CASE("MakeVariant", "[VariantUtility]")
-{
-    auto e0 = MakeVariant(1);
-    auto e1 = MakeVariant(1);
-    REQUIRE(boost::apply_visitor(IsEqual(), e0, e1));
-
-    auto e2 = MakeVariant('a');
-    REQUIRE(!boost::apply_visitor(IsEqual(), e0, e2));
-}
-
 TEST_CASE("TupleToVariant", "[VariantUtility]")
 {
     using std::is_same;
@@ -48,6 +38,72 @@ TEST_CASE("TupleToVariant", "[VariantUtility]")
             is_same<variant_t, typename TupleToVariant<tuple_t>::type>(),
             "converted tuple_t shoulb be same as variant_t."
     );
+}
+
+TEST_CASE("CharPtrTypeToStrType", "[VariantUtility]")
+{
+    using std::is_same;
+
+    {
+        char a[] = "abc";
+        static_assert(is_same<decltype(a), char [4]>(), "");
+        static_assert(is_same<std::string, typename CharPtrTypeToStrType<decltype(a)>::type>(), "");
+
+        wchar_t wa[] = L"abc";
+        static_assert(is_same<decltype(wa), wchar_t [4]>(), "");
+        static_assert(is_same<std::wstring, typename CharPtrTypeToStrType<decltype(wa)>::type>(), "");
+    }
+
+    {
+        using str_t = decltype("abc");
+        static_assert(is_same<str_t, char const (&)[4]>(), "");
+        static_assert(is_same<std::string, typename CharPtrTypeToStrType<str_t>::type>(), "");
+
+        using wstr_t = decltype(L"abc");
+        static_assert(is_same<wstr_t, wchar_t const (&)[4]>(), "");
+        static_assert(is_same<std::wstring, typename CharPtrTypeToStrType<wstr_t>::type>(), "");
+    }
+
+    {
+        char const * str = "abc";
+        static_assert(is_same<decltype(str), char const *>(), "");
+        static_assert(is_same<std::string, typename CharPtrTypeToStrType<decltype(str)>::type>(), "");
+
+        wchar_t const * wstr = L"abc";
+        static_assert(is_same<decltype(wstr), wchar_t const *>(), "");
+        static_assert(is_same<std::wstring, typename CharPtrTypeToStrType<decltype(wstr)>::type>(), "");
+    }
+
+    {
+        char a[] = "abc";
+        char * pa = a;
+        static_assert(is_same<decltype(pa), char *>(), "");
+        static_assert(is_same<std::string, typename CharPtrTypeToStrType<decltype(pa)>::type>(), "");
+
+        wchar_t wa[] = L"abc";
+        wchar_t * pwa = wa;
+        static_assert(is_same<decltype(pwa), wchar_t *>(), "");
+        static_assert(is_same<std::wstring, typename CharPtrTypeToStrType<decltype(pwa)>::type>(), "");
+    }
+}
+
+TEST_CASE("MakeVariant", "[VariantUtility]")
+{
+    auto e0 = MakeVariant(1);
+    auto e1 = MakeVariant(1);
+    REQUIRE(boost::apply_visitor(IsEqual(), e0, e1));
+
+    auto e2 = MakeVariant('a');
+    REQUIRE(!boost::apply_visitor(IsEqual(), e0, e2));
+
+    auto e3 = MakeVariant("abc");
+    auto e4 = MakeVariant(L"abc");
+    REQUIRE(!boost::apply_visitor(IsEqual(), e3, e4));
+
+    auto e5 = MakeVariant("abc");
+    auto e6 = MakeVariant("ABC");
+    REQUIRE(boost::apply_visitor(IsEqual(), e3, e5));
+    REQUIRE(!boost::apply_visitor(IsEqual(), e3, e6));
 }
 
 TEST_CASE("MakeVariantVector, basic", "[VariantUtility]")
@@ -92,5 +148,60 @@ TEST_CASE("MakeVariantVector, duplicate parameter types", "[VariantUtility]")
     REQUIRE(boost::apply_visitor(IsEqual(), vv[2], e2));
     REQUIRE(boost::apply_visitor(IsEqual(), vv[3], e3));
     REQUIRE(boost::apply_visitor(IsEqual(), vv[4], e4));
+}
+
+TEST_CASE("MakeVariantVector, char * types", "[VariantUtility]")
+{
+    using std::is_same;
+    using std::decay_t;
+    using std::vector;
+    using boost::variant;
+
+    {
+        auto && vv = MakeVariantVector(1, 1.0, 'a', 2, 'b', "abc");
+        using result_t = vector<variant<int, double, char, std::string>>;
+        static_assert(is_same<decay_t<decltype(vv)>, result_t>(), "vv's type should be result_t.");
+        REQUIRE(vv.size() == 6);
+
+        auto && e0 = MakeVariant(1);
+        auto && e1 = MakeVariant(1.0);
+        auto && e2 = MakeVariant('a');
+        auto && e3 = MakeVariant(2);
+        auto && e4 = MakeVariant('b');
+        auto && e5 = MakeVariant("abc");
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[0], e0));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[1], e1));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[2], e2));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[3], e3));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[4], e4));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[5], e5));
+    }
+
+    {
+        auto && vv = MakeVariantVector(1, 1.0, 'a', 2, 'b', "abc", float(3), L"zxy", "zxy");
+        using result_t = vector<variant<int, double, char, std::string, float, std::wstring>>;
+        static_assert(is_same<decay_t<decltype(vv)>, result_t>(), "vv's type should be result_t.");
+        REQUIRE(vv.size() == 9);
+
+        auto && e0 = MakeVariant(1);
+        auto && e1 = MakeVariant(1.0);
+        auto && e2 = MakeVariant('a');
+        auto && e3 = MakeVariant(2);
+        auto && e4 = MakeVariant('b');
+        auto && e5 = MakeVariant("abc");
+        auto && e6 = MakeVariant(float(3));
+        auto && e7 = MakeVariant(L"zxy");
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[0], e0));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[1], e1));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[2], e2));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[3], e3));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[4], e4));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[5], e5));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[6], e6));
+        REQUIRE(boost::apply_visitor(IsEqual(), vv[7], e7));
+
+        REQUIRE(!boost::apply_visitor(IsEqual(), vv[1], e0));
+        REQUIRE(!boost::apply_visitor(IsEqual(), vv[8], e7));
+    }
 }
 
