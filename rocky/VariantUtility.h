75aa2224e9711d7e8040683624239f6b920d7c0d
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-06 01:24:16 +0900

    made it handle the duplicate argument type case.

--- rocky/VariantUtility.h
+++ rocky/VariantUtility.h
@@ -8,6 +8,17 @@
 #include "boost/variant.hpp"
 
 #include "rocky/meta/ForEachArgument.h"
+#include "rocky/meta/UniqueTuple.h"
+
+
+template <typename Tuple>
+struct TupleToVariant;
+
+template <typename... list>
+struct TupleToVariant<std::tuple<list...>>
+{
+    using type = boost::variant<list...>;
+};
 
 
 template <typename T>
@@ -20,7 +31,8 @@ decltype(auto) MakeVariant(const T && arg)
 template <typename... Args>
 decltype(auto) MakeVariantVector(const Args &&...  args)
 {
-    using element_t = boost::variant<std::decay_t<decltype(args)>...>;
+    using unique_tuple_t = typename MakeUniqueElementTypeTuple<std::decay_t<decltype(args)>...>::type;
+    using element_t = typename TupleToVariant<unique_tuple_t>::type;
 
     std::vector<element_t> v;
     v.reserve(sizeof...(args));
--- rocky/test/VariantUtilityTest.cpp
+++ rocky/test/VariantUtilityTest.cpp
@@ -26,7 +26,31 @@ namespace
 }
 
 
-TEST_CASE("MakeVariantVector function template basic", "[VariantUtility]")
+TEST_CASE("MakeVariant", "[VariantUtility]")
+{
+    auto e0 = MakeVariant(1);
+    auto e1 = MakeVariant(1);
+    REQUIRE(boost::apply_visitor(IsEqual(), e0, e1));
+
+    auto e2 = MakeVariant('a');
+    REQUIRE(!boost::apply_visitor(IsEqual(), e0, e2));
+}
+
+TEST_CASE("TupleToVariant", "[VariantUtility]")
+{
+    using std::is_same;
+    using std::tuple;
+    using boost::variant;
+
+    using tuple_t = tuple<char, int, double>;
+    using variant_t = variant<char, int, double>;
+    static_assert(
+            is_same<variant_t, typename TupleToVariant<tuple_t>::type>(),
+            "converted tuple_t shoulb be same as variant_t."
+    );
+}
+
+TEST_CASE("MakeVariantVector, basic", "[VariantUtility]")
 {
     using std::is_same;
     using std::decay_t;
@@ -44,5 +68,29 @@ TEST_CASE("MakeVariantVector function template basic", "[VariantUtility]")
     REQUIRE(boost::apply_visitor(IsEqual(), vv[0], e0));
     REQUIRE(boost::apply_visitor(IsEqual(), vv[1], e1));
     REQUIRE(boost::apply_visitor(IsEqual(), vv[2], e2));
+}
+
+TEST_CASE("MakeVariantVector, duplicate parameter types", "[VariantUtility]")
+{
+    using std::is_same;
+    using std::decay_t;
+    using std::vector;
+    using boost::variant;
+
+    auto && vv = MakeVariantVector(1, 1.0, 'a', 2, 'b');
+    using result_t = vector<variant<int, double, char>>;
+    static_assert(is_same<decay_t<decltype(vv)>, result_t>(), "vv's type should be result_t.");
+    REQUIRE(vv.size() == 5);
+
+    auto && e0 = MakeVariant(1);
+    auto && e1 = MakeVariant(1.0);
+    auto && e2 = MakeVariant('a');
+    auto && e3 = MakeVariant(2);
+    auto && e4 = MakeVariant('b');
+    REQUIRE(boost::apply_visitor(IsEqual(), vv[0], e0));
+    REQUIRE(boost::apply_visitor(IsEqual(), vv[1], e1));
+    REQUIRE(boost::apply_visitor(IsEqual(), vv[2], e2));
+    REQUIRE(boost::apply_visitor(IsEqual(), vv[3], e3));
+    REQUIRE(boost::apply_visitor(IsEqual(), vv[4], e4));
 }
 
