742152030032af2a6cce25d4d76abbe7dfbb9680
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-18 12:05:06 +0900

    added type_is type alias and test.

--- rocky/meta/Identity.h
+++ rocky/meta/Identity.h
@@ -9,4 +9,10 @@ struct Identity
 };
 
 
+// NOTE: type_is is more descriptive than Identity in some cases.
+template <typename T>
+using type_is = Identity<T>;
+
+
 #endif //ROCKY_IDENTITY_H
+
--- rocky/test/IdentityTest.cpp
+++ rocky/test/IdentityTest.cpp
@@ -24,3 +24,20 @@ TEST_CASE("Wrapping as type member", "[Identity]")
     static_assert(is_same<WrappedB::type::type, int>(), "WrappedB::type::type should be int.");
 }
 
+TEST_CASE("type_is", "[Identity]")
+{
+    using std::is_same;
+
+    struct A { };
+    struct B { using type = int; };
+
+    using WrappedA = type_is<A>;
+    static_assert(HasTypeMember<WrappedA>(), "WrappedA should have 'type' member.");
+    static_assert(is_same<WrappedA::type, A>(), "WrappedA::type should be A.");
+
+    using WrappedB = type_is<B>;
+    static_assert(HasTypeMember<WrappedB>(), "WrappedB should have 'type' member.");
+    static_assert(is_same<WrappedB::type, B>(), "WrappedB::type should be B.");
+    static_assert(is_same<WrappedB::type::type, int>(), "WrappedB::type::type should be int.");
+}
+
