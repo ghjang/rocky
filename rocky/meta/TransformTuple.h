0e11a0064552c1f09ed6325f1c04df40668312e3
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-18 12:42:06 +0900

    applied move semantics.

--- rocky/meta/TransformTuple.h
+++ rocky/meta/TransformTuple.h
@@ -180,15 +180,15 @@ struct ExtractElementType<std::tuple<list...>, std::integer_sequence<T, i...>>
 //==============================================================================
 
 template <typename... list, typename F, std::size_t... i>
-decltype(auto) TransformElementImpl(std::tuple<list...> const& t, F const& f, std::index_sequence<i...>)
+auto TransformElementImpl(std::tuple<list...> const& t, F && f, std::index_sequence<i...>)
 {
     return std::make_tuple(f(std::get<i>(t))...);
 }
 
 template <typename... list, typename F>
-decltype(auto) TransformElement(std::tuple<list...> const& t, F const& f)
+auto TransformElement(std::tuple<list...> const& t, F && f)
 {
-    return TransformElementImpl(t, f, std::index_sequence_for<list...>{});
+    return TransformElementImpl(t, std::forward<F>(f), std::index_sequence_for<list...>{});
 }
 
 
