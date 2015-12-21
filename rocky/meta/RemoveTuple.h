69c69ebc3b9a8f21bd21da22c11fe3cf93b743c5
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-21 11:57:07 +0900

    re-implemented RemoveElementType by using Fold.
    - made it simpler.

--- rocky/meta/RemoveTuple.h
+++ rocky/meta/RemoveTuple.h
@@ -2,12 +2,45 @@
 #define ROCKY_REMOVETUPLE_H
 
 
-#include "rocky/meta/TransformTuple.h"
+#include "rocky/meta/TypeUtility.h"
+#include "rocky/meta/Fold.h"
+
+//#include "rocky/meta/TransformTuple.h"
 
 
 template <template <typename> class Predicate, typename Tuple>
 struct RemoveElementType;
 
+template <template <typename> class Predicate, typename... list>
+struct RemoveElementType<Predicate, std::tuple<list...>>
+{
+private:
+    static_assert(HasValueMember<Predicate<int>>(), "Predicate should have 'value' member.");
+
+    using init_t = std::tuple<>;
+
+    template <typename lhsTuple, typename rhsType>
+    struct AppendTypeIf;
+
+    template <typename... lhsList, typename rhsType>
+    struct AppendTypeIf<std::tuple<lhsList...>, rhsType>
+            : std::conditional<
+                    Predicate<rhsType>::value,
+                    std::tuple<lhsList...>,
+                    std::tuple<lhsList..., rhsType>
+            >
+    { };
+
+public:
+    using type = typename FoldLeft<AppendTypeIf, init_t, list...>::type;
+};
+
+
+/**
+ * NOTE: Following implementation also works. But, it's more complicated.
+ *          The usage example of 'Transform' is still useful for reference.
+ */
+/*
 template <template <typename> class Predicate, typename... list>
 struct RemoveElementType<Predicate, std::tuple<list...>>
 {
@@ -20,6 +53,7 @@ private:
 public:
     using type = typename ExtractElementType<target_element_index_seq_t, std::tuple<list...>>::type;
 };
+*/
 
 
 #endif //ROCKY_REMOVETUPLE_H
