016063d3c1865d6afff8d8e6eba422aee9e267ed
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-24 20:54:50 +0900

    made IntegerDigits function to function template to make it header-file-only.
    - added IntegerDigitCharacterT metafunction and it’s implementation
    codes.

--- rocky/wolca/IntegerDigits.h
+++ rocky/wolca/IntegerDigits.h
@@ -7,7 +7,8 @@
 #include "rocky/wolca/IntegerLength.h"
 
 
-auto IntegerDigits(intmax_t n)
+template <typename T>
+auto IntegerDigits(T n)
 {
     if (n < 0) {
         n = -n;
@@ -21,6 +22,41 @@ auto IntegerDigits(intmax_t n)
     digits[0] = n;
     return digits;
 }
+
+template <typename T>
+constexpr auto Abs(T t)
+{
+    return (t < 0) ? -t : t;
+}
+
+constexpr auto IntegerDigit(intmax_t n, uint8_t numOfDigit, uint8_t digitIndex)
+{
+    intmax_t modNum = 1;
+    for (uint8_t i = 0; i < digitIndex; ++i) {
+        modNum *= 10;
+    }
+    n %= modNum;
+
+    for (uint8_t i = 0; i < digitIndex - 1; ++i) {
+        n /= 10;
+    }
+
+    return Abs(n);
+}
+
+template <intmax_t N, std::size_t DigitIndex>
+struct IntegerDigitT
+{
+    static_assert(DigitIndex <= IntegerLength(N), "");
+    static constexpr int8_t value = IntegerDigit(N, IntegerLength(N), DigitIndex);
+};
+
+template <intmax_t N, std::size_t DigitIndex>
+struct IntegerDigitCharacterT
+{
+    static_assert(DigitIndex <= IntegerLength(N), "");
+    static constexpr char value = '0' + IntegerDigitT<N, DigitIndex>::value;
+};
 
 
 #endif //ROCKY_INTEGERDIGITS_H
