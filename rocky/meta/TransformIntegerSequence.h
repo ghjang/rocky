ec9b2fd2f22877230e29f409ac332242fe41969b
Gil Ho, Jang <jmxexpert@hotmail.com>
2015-12-25 13:54:07 +0900

    added TransformIntegerSequence metafunction and tests.

---
+++ rocky/meta/TransformIntegerSequence.h
@@ -0,0 +1,68 @@
+#ifndef ROCKY_TRANSFORMINTEGERSEQUENCE_H
+#define ROCKY_TRANSFORMINTEGERSEQUENCE_H
+
+
+#include <type_traits>
+#include <utility>
+
+#include "rocky/meta/HasMember.h"
+#include "rocky/meta/IntegerSequenceUtility.h"
+
+
+namespace Detail
+{
+    template<template<std::size_t> class F, typename IntegerSequence, typename IndexSequence>
+    struct TransformIntegerSequenceImpl;
+
+    template<template<std::size_t> class F, typename T, T... list, std::size_t... i>
+    struct TransformIntegerSequenceImpl<F, std::integer_sequence<T, list...>, std::index_sequence<i...>>
+    {
+        using type = std::integer_sequence<
+                            T,
+                            F<IntegerSequenceValue<i, std::integer_sequence<T, list...>>::value>::value...
+                        >;
+    };
+} // namespace Detail
+
+
+template <template <std::size_t> class F, typename IntegerSequence>
+struct TransformIntegerSequence;
+
+template <template <std::size_t> class F, typename T>
+struct TransformIntegerSequence<F, std::integer_sequence<T>>
+{
+private:
+    template <typename IntSeq>
+    struct IntSeqLen;
+
+    template <T... i>
+    struct IntSeqLen<std::integer_sequence<T, i...>>
+            : std::integral_constant<T, sizeof...(i)>
+    { };
+
+    static_assert(
+            IntSeqLen<std::integer_sequence<T>>() > 0, "integer sequence should have at least one integer value."
+    );
+};
+
+template <template <std::size_t> class F, typename T, T first, T... list>
+struct TransformIntegerSequence<F, std::integer_sequence<T, first, list...>>
+            : Detail::TransformIntegerSequenceImpl<
+                    F,
+                    std::integer_sequence<T, first, list...>,
+                    std::make_index_sequence<1 + sizeof...(list)>
+                >
+{
+    static_assert(HasValueMember<F<first>>(), "F should have 'value' member.");
+};
+
+
+template <template<std::size_t> class GeneratorFunc, std::size_t i>
+using MakeCustomIntegerSequence = typename TransformIntegerSequence<
+                                                GeneratorFunc,
+                                                std::make_index_sequence<i>
+                                            >::type;
+
+
+#endif //ROCKY_TRANSFORMINTEGERSEQUENCE_H
+
---
+++ rocky/test/TransformIntegerSequenceTest.cpp
@@ -0,0 +1,62 @@
+#include "catch.hpp"
+
+#include "rocky/meta/TransformIntegerSequence.h"
+
+
+namespace
+{
+    constexpr int SquareInt(int i) { return i * i; }
+
+    template <std::size_t i>
+    struct SquareIntGenerator
+    {
+        constexpr static std::size_t value = SquareInt(i);
+    };
+
+    constexpr int Factorial(int i)
+    {
+        if (i == 0) {
+            return 1;
+        }
+        return i * Factorial(i - 1);
+    }
+
+    template <std::size_t i>
+    struct FactorialGenerator
+    {
+        constexpr static std::size_t value = Factorial(i);
+    };
+}
+
+
+TEST_CASE("TransformIntegerSequence", "[TransformIntegerSequence]")
+{
+    // NOTE: this is an expected static assertion failure.
+    //typename TransformIntegerSequence<SquareIntGenerator, std::integer_sequence<std::size_t>>::type();
+}
+
+TEST_CASE("squared integer sequence", "[TransformIntegerSequence]")
+{
+    using std::is_same;
+    using std::integer_sequence;
+
+    using square_int_seq = typename TransformIntegerSequence<
+                                            SquareIntGenerator,
+                                            std::make_integer_sequence<std::size_t, 4>
+                                        >::type;
+    static_assert(is_same<integer_sequence<std::size_t, 0, 1, 4, 9>, square_int_seq>(), "");
+
+    using square_int_seq_1 = MakeCustomIntegerSequence<SquareIntGenerator, 4>;
+    static_assert(is_same<integer_sequence<std::size_t, 0, 1, 4, 9>, square_int_seq_1>(), "");
+    static_assert(is_same<square_int_seq, square_int_seq_1>(), "");
+}
+
+TEST_CASE("factorial integer sequence", "[TransformIntegerSequence]")
+{
+    using std::is_same;
+    using std::integer_sequence;
+
+    using factorial_int_seq = MakeCustomIntegerSequence<FactorialGenerator, 5>;
+    static_assert(is_same<integer_sequence<std::size_t, 1, 1, 2, 6, 24>, factorial_int_seq>(), "");
+}
+
