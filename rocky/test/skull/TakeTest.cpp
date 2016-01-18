df1d6e42c0233ca65ad1f2fbe07c5151ddc10837
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-18 23:12:06 +0900

    updated test.

--- rocky/test/skull/TakeTest.cpp
+++ rocky/test/skull/TakeTest.cpp
@@ -9,10 +9,20 @@ TEST_CASE("Take", "[skull]")
 {
     using std::is_same;
 
+    //
+    static_assert(is_same<TypeList<>, typename Take<0>::type>(), "");
+    static_assert(is_same<TypeList<>, typename Take<0, TypeList<>>::type>(), "");
+
+    //
+    static_assert(is_same<TypeList<>, typename Take<0, char, int, float, double>::type>(), "");
+    static_assert(is_same<TypeList<char>, typename Take<1, char, int, float, double>::type>(), "");
+    static_assert(is_same<TypeList<char, int>, typename Take<2, char, int, float, double>::type>(), "");
+    static_assert(is_same<TypeList<char, int, float, double>, typename Take<5, char, int, float, double>::type>(), "");
+
+    //
     static_assert(
             is_same<TypeList<>, typename Take<0, TypeList<char, int, float, double>>::type>(), ""
     );
-    /*
     static_assert(
             is_same<TypeList<char>, typename Take<1, TypeList<char, int, float, double>>::type>(), ""
     );
@@ -26,6 +36,5 @@ TEST_CASE("Take", "[skull]")
             >(),
             ""
     );
-     */
 }
 
