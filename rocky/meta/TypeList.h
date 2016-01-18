2a84dad7598200a5ede65f4db98fdf56a684bb72
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-18 21:31:21 +0900

    added JoinTypeList metafunction and test.

--- rocky/meta/TypeList.h
+++ rocky/meta/TypeList.h
@@ -2,8 +2,49 @@
 #define ROCKY_TYPELIST_H
 
 
+#include "rocky/meta/Identity.h"
+
+
 template <typename... T>
-struct TypeList;
+struct TypeList
+{ };
+
+
+template <typename... T>
+struct JoinTypeList;
+
+template <typename... T1, typename... T2>
+struct JoinTypeList<TypeList<T1...>, TypeList<T2...>> : type_is<TypeList<T1..., T2...>>
+{ };
+
+template <typename... T1, typename... T2>
+struct JoinTypeList<TypeList<T1...>, T2...> : type_is<TypeList<T1..., T2...>>
+{ };
+
+/**
+ * following is not a valid pattern matching.
+ */
+/*
+template <typename... T1, typename... T2>
+struct JoinTypeList<T1..., TypeList<T2...>> : type_is<TypeList<T1..., T2...>>
+{ };
+ */
+
+/**
+ * following is not a valid pattern matching.
+ */
+/*
+template <typename... T1, typename... T2, typename... T3>
+struct JoinTypeList<T1..., TypeList<T2...>, T3...> : type_is<TypeList<T1..., T2..., T3...>>
+{ };
+ */
+
+
+template <typename... T1, typename... T2>
+constexpr auto operator + (TypeList<T1...>, TypeList<T2...>)
+{
+    return TypeList<T1..., T2...>{};
+}
 
 
 #endif //ROCKY_TYPELIST_H
--- rocky/test/meta/TypeListTest.cpp
+++ rocky/test/meta/TypeListTest.cpp
@@ -2,8 +2,52 @@
 
 #include "rocky/meta/TypeList.h"
 
+#include <type_traits>
 
-TEST_CASE("TypeList", "[TypeList]")
+
+TEST_CASE("TypeList join", "[TypeList]")
 {
-    // NOTE: no need to write tests for TypeList class template.
+    using std::is_same;
+
+    static_assert(
+            is_same<
+                TypeList<char, int, float, double>,
+                typename JoinTypeList<TypeList<char, int>, TypeList<float, double>>::type
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                TypeList<char, int, float, double>,
+                typename JoinTypeList<TypeList<char, int>, float, double>::type
+            >(),
+            ""
+    );
+
+    /**
+     * following is not a valid pattern matching.
+     */
+    /*
+    static_assert(
+            is_same<
+                    TypeList<char, int, float, double>,
+                    typename JoinTypeList<char, int, TypeList<float, double>>::type
+            >(),
+            ""
+    );
+     */
 }
+
+TEST_CASE("TypeList operator +", "[TypeList]")
+{
+    using std::is_same;
+    using std::decay_t;
+
+    constexpr auto crt = TypeList<char, int>{} + TypeList<float, double>{};
+    static_assert(is_same<decay_t<decltype(crt)>, TypeList<char, int, float, double>>(), "");
+
+    auto rt = TypeList<char, int>{} + TypeList<float, double>{};
+    static_assert(is_same<decltype(rt), TypeList<char, int, float, double>>(), "");
+}
+
