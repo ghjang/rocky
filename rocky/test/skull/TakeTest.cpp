0b17c65a926b867296bc66b547fde07c4f59262e
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-19 00:04:40 +0900

    fixed broken build.

--- rocky/test/skull/TakeTest.cpp
+++ rocky/test/skull/TakeTest.cpp
@@ -10,19 +10,19 @@ TEST_CASE("Take", "[skull]")
     using std::is_same;
 
     //
-    static_assert(is_same<TypeList<>, TakeT<0>(), "");
-    static_assert(is_same<TypeList<>, TakeT<0, TypeList<>>(), "");
+    static_assert(is_same<TypeList<>, TakeT<0>>(), "");
+    static_assert(is_same<TypeList<>, TakeT<0, TypeList<>>>(), "");
 
     //
-    static_assert(is_same<TypeList<>, TakeT<0, char, int, float, double>(), "");
-    static_assert(is_same<TypeList<char>, TakeT<1, char, int, float, double>(), "");
-    static_assert(is_same<TypeList<char, int>, TakeT<2, char, int, float, double>(), "");
-    static_assert(is_same<TypeList<char, int, float, double>, TakeT<5, char, int, float, double>(), "");
+    static_assert(is_same<TypeList<>, TakeT<0, char, int, float, double>>(), "");
+    static_assert(is_same<TypeList<char>, TakeT<1, char, int, float, double>>(), "");
+    static_assert(is_same<TypeList<char, int>, TakeT<2, char, int, float, double>>(), "");
+    static_assert(is_same<TypeList<char, int, float, double>, TakeT<5, char, int, float, double>>(), "");
 
     //
-    static_assert(is_same<TypeList<>, TakeT<0, TypeList<char, int, float, double>>(), "");
-    static_assert(is_same<TypeList<char>, TakeT<1, TypeList<char, int, float, double>>(), "");
-    static_assert(is_same<TypeList<char, int>, TakeT<2, TypeList<char, int, float, double>>(), "");
-    static_assert(is_same<TypeList<char, int, float, double>, TakeT<5, TypeList<char, int, float, double>>(), "");
+    static_assert(is_same<TypeList<>, TakeT<0, TypeList<char, int, float, double>>>(), "");
+    static_assert(is_same<TypeList<char>, TakeT<1, TypeList<char, int, float, double>>>(), "");
+    static_assert(is_same<TypeList<char, int>, TakeT<2, TypeList<char, int, float, double>>>(), "");
+    static_assert(is_same<TypeList<char, int, float, double>, TakeT<5, TypeList<char, int, float, double>>>(), "");
 }
 
