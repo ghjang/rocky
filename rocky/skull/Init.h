0eb1c7877516182a1ba0893fb78b030d53a9a952
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-23 00:04:16 +0900

    added Init metafunction and test.

---
+++ rocky/skull/Init.h
@@ -0,0 +1,43 @@
+#ifndef ROCKY_SKULL_INIT_H
+#define ROCKY_SKULL_INIT_H
+
+
+#include "rocky/meta/TypeList.h"
+#include "rocky/meta/Identity.h"
+#include "rocky/skull/Take.h"
+
+
+template <typename... xs>
+struct Init
+{
+    static_assert(sizeof...(xs) > 0, "empty type list is not allowed.");
+};
+
+/*
+ * NOTE: following is not a valid codes.
+ */
+/*
+template <typename... xs, typename x>
+struct Init<xs..., x> : type_is<TypeList<xs...>>
+{ };
+ */
+
+template <typename x, typename... xs>
+struct Init<x, xs...> : Take<sizeof...(xs), x, xs...>
+{ };
+
+template <typename... xs>
+struct Init<TypeList<xs...>> : Init<xs...>
+{ };
+
+template <typename... xs>
+struct Init<std::tuple<xs...>> : Init<xs...>
+{ };
+
+
+template <typename... xs>
+using InitT = typename Init<xs...>::type;
+
+
+#endif //ROCKY_SKULL_INIT_H
+
---
+++ rocky/test/skull/InitTest.cpp
@@ -0,0 +1,24 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/Init.h"
+
+
+TEST_CASE("Init", "[skull]")
+{
+    using std::is_same;
+    using std::tuple;
+
+    // empty type list. compile-time error.
+    //static_assert(!is_same<int, InitT<>>(), "");
+
+    static_assert(is_same<TypeList<>, InitT<int>>(), "");
+
+    static_assert(is_same<TypeList<int>, InitT<int, float>>(), "");
+
+    static_assert(is_same<TypeList<int, float>, InitT<int, float, double>>(), "");
+
+    static_assert(is_same<TypeList<int, float>, InitT<TypeList<int, float, double>>>(), "");
+
+    static_assert(is_same<TypeList<int, float>, InitT<tuple<int, float, double>>>(), "");
+}
+
