97aff5a0d967ed6c29f0c565e4ea54a342a9f1ea
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-23 18:13:31 +0900

    implemented Filter metafunction.
    - added test.

--- rocky/skull/Filter.h
+++ rocky/skull/Filter.h
@@ -2,11 +2,43 @@
 #define ROCKY_SKULL_FILTER_H
 
 
-#include "rocky/meta/TypeList.h"
+#include "rocky/meta/TypeListJoin.h"
+#include "rocky/meta/TypeSelection.h"
+#include "rocky/skull/FoldL.h"
 
 
 template <template <typename> class p, typename... xs>
-struct Filter;
+struct Filter
+{
+private:
+    using init_t = TypeList<>;
+
+    template <typename lhs, typename rhs>
+    struct AppendIfTrue : SelectTypeIf<
+                                p<rhs>::value,
+                                JoinTypeList<lhs, rhs>,
+                                lhs
+                          >
+    {
+        static_assert(HasValueMember<p<rhs>>(), "Predicate p should have 'value' member.");
+    };
+
+public:
+    using type = FoldLT<AppendIfTrue, init_t, xs...>;
+};
+
+
+template <template <typename> class p, typename... xs>
+using FilterT = typename Filter<p, xs...>::type;
+
+
+template <template <typename> class p, typename... xs>
+struct Filter<p, TypeList<xs...>> : Filter<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct Filter<p, std::tuple<xs...>> : TypeListToTuple<FilterT<p, xs...>>
+{ };
 
 
 #endif //ROCKY_SKULL_FILTER_H
--- rocky/test/skull/FilterTest.cpp
+++ rocky/test/skull/FilterTest.cpp
@@ -9,6 +9,18 @@ TEST_CASE("Filter", "[skull]")
     using std::is_integral;
     using std::tuple;
 
-    //static_assert(is_same<TypeList<char, int, long>, FilterT<is_integral, char, float, int, double, long>>(), "");
+    static_assert(is_same<TypeList<>, FilterT<is_integral>>(), "");
+    static_assert(is_same<TypeList<>, FilterT<is_integral, TypeList<>>>(), "");
+    static_assert(is_same<tuple<>, FilterT<is_integral, tuple<>>>(), "");
+
+    static_assert(is_same<TypeList<char, int, long>, FilterT<is_integral, char, float, int, double, long>>(), "");
+    static_assert(
+            is_same<TypeList<char, int, long>, FilterT<is_integral, TypeList<char, float, int, double, long>>>(),
+            ""
+    );
+    static_assert(
+            is_same<tuple<char, int, long>, FilterT<is_integral, tuple<char, float, int, double, long>>>(),
+            ""
+    );
 }
 
