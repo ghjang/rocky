743f852a20112e6f8f4b2d2d42bc1d05351e7505
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-23 12:54:42 +0900

    added Map metafunction and test.

---
+++ rocky/skull/Map.h
@@ -0,0 +1,37 @@
+#ifndef ROCKY_SKULL_MAP_H
+#define ROCKY_SKULL_MAP_H
+
+
+#include <tuple>
+
+#include "rocky/meta/Identity.h"
+#include "rocky/meta/TypeList.h"
+#include "rocky/meta/HasMember.h"
+#include "rocky/skull/Head.h"
+
+
+template <template <typename> class f, typename... xs>
+struct Map : type_is<TypeList<typename f<xs>::type...>>
+{
+    static_assert(HasTypeMember<f<HeadT<xs...>>>(), "f should have 'type' member.");
+};
+
+template <template <typename> class f>
+struct Map<f> : type_is<TypeList<>>
+{ };
+
+template <template <typename> class f, typename... xs>
+struct Map<f, TypeList<xs...>> : Map<f, xs...>
+{ };
+
+template <template <typename> class f, typename... xs>
+struct Map<f, std::tuple<xs...>> : Map<f, xs...>
+{ };
+
+
+template <template <typename> class f, typename... xs>
+using MapT = typename Map<f, xs...>::type;
+
+
+#endif //ROCKY_SKULL_MAP_H
+
---
+++ rocky/test/skull/MapTest.cpp
@@ -0,0 +1,35 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/Map.h"
+
+
+TEST_CASE("Map", "[skull]")
+{
+    using std::is_same;
+    using std::tuple;
+    using std::add_pointer;
+
+    static_assert(is_same<TypeList<>, MapT<add_pointer>>(), "");
+    static_assert(is_same<TypeList<>, MapT<add_pointer, TypeList<>>>(), "");
+    static_assert(is_same<TypeList<>, MapT<add_pointer, tuple<>>>(), "");
+
+    static_assert(
+            is_same<TypeList<char *, int *, float *, double *>, MapT<add_pointer, char, int, float, double>>(),
+            ""
+    );
+    static_assert(
+            is_same<
+                    TypeList<char *, int *, float *, double *>,
+                    MapT<add_pointer, TypeList<char, int, float, double>>
+            >(),
+            ""
+    );
+    static_assert(
+            is_same<
+                    TypeList<char *, int *, float *, double *>,
+                    MapT<add_pointer, tuple<char, int, float, double>>
+            >(),
+            ""
+    );
+}
+
