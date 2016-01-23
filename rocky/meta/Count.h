14a9c73cd9c399c8a0a4477b3d85946d7efa946e
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-23 21:28:34 +0900

    rewrote Count by using Sum.

--- rocky/meta/Count.h
+++ rocky/meta/Count.h
@@ -3,18 +3,11 @@
 
 
 #include "rocky/meta/Transform.h"
-#include "rocky/meta/IntegralConstantUtility.h"
-#include "rocky/skull/FoldR.h"
+#include "rocky/skull/Sum.h"
 
 
 template <template <typename> class p, typename... xs>
-struct Count
-        : FoldRT<
-                Plus,
-                int_c_t<0>, // init
-                MapToBoolConstantTypeT<p, xs...>
-          >
-{ };
+using Count = Sum<MapToBoolConstantTypeT<p, xs...>>;
 
 
 #endif //ROCKY_COUNT_H
