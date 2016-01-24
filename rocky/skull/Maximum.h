406e8d4b1068b60c3aadf50a443b16d6bbf05daa
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-25 01:25:42 +0900

    added Maximum metafunction and test.

---
+++ rocky/skull/Maximum.h
@@ -0,0 +1,40 @@
+#ifndef ROCKY_SKULL_MAXIMUM_H
+#define ROCKY_SKULL_MAXIMUM_H
+
+
+#include "rocky/meta/TypeList.h"
+#include "rocky/meta/IntegralConstantUtility.h"
+#include "rocky/skull/FoldR.h"
+
+
+template <typename... xs>
+struct Maximum
+{
+    static_assert(sizeof...(xs) > 0, "empty type list is not allowed.");
+};
+
+template <typename T, T... i>
+struct Maximum<std::integral_constant<T, i>...>
+        : FoldR<
+                Max,
+                std::integral_constant<T, std::numeric_limits<T>::min()>, // init
+                std::integral_constant<T, i>...
+            >
+{ };
+
+
+template <typename... xs>
+using MaximumT = typename Maximum<xs...>::type;
+
+
+template <typename... xs>
+struct Maximum<TypeList<xs...>> : Maximum<xs...>
+{ };
+
+template <typename... xs>
+struct Maximum<std::tuple<xs...>> : Maximum<xs...>
+{ };
+
+
+#endif //ROCKY_SKULL_MAXIMUM_H
+
---
+++ rocky/test/skull/MaximumTest.cpp
@@ -0,0 +1,25 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/Maximum.h"
+
+
+TEST_CASE("Maximum for std::integral_constant", "[skull]")
+{
+    using std::is_same;
+    using std::tuple;
+
+    // NOTE: following are expected compile-time errors.
+    //static_assert(0 == MaximumT<>(), "");
+    //static_assert(0 == MaximumT<TypeList<>>(), "");
+    //static_assert(0 == MaximumT<tuple<>>(), "");
+
+    static_assert(2 == MaximumT<int_c_t<0>, int_c_t<2>, int_c_t<1>>(), "");
+    static_assert(is_same<int_c_t<2>, MaximumT<int_c_t<0>, int_c_t<2>, int_c_t<1>>>(), "");
+
+    static_assert(2 == MaximumT<TypeList<int_c_t<0>, int_c_t<2>, int_c_t<1>>>(), "");
+    static_assert(is_same<int_c_t<2>, MaximumT<TypeList<int_c_t<0>, int_c_t<2>, int_c_t<1>>>>(), "");
+
+    static_assert(2 == MaximumT<tuple<int_c_t<0>, int_c_t<2>, int_c_t<1>>>(), "");
+    static_assert(is_same<int_c_t<2>, MaximumT<tuple<int_c_t<0>, int_c_t<2>, int_c_t<1>>>>(), "");
+}
+
