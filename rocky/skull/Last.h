abccca4ad2c88ec02f7a5061354e4aeb5fc7b5a4
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-25 00:50:57 +0900

    added support for TypeList and std::tuple.
    - updated tests.

--- rocky/skull/Last.h
+++ rocky/skull/Last.h
@@ -8,7 +8,21 @@
 
 
 template <typename... xs>
-using Last = HeadT<DropT<(sizeof...(xs) <= 0) ? 0 : (sizeof...(xs) - 1), xs...>>;
+struct Last : Head<DropT<(sizeof...(xs) <= 0) ? 0 : (sizeof...(xs) - 1), xs...>>
+{ };
+
+
+template <typename... xs>
+using LastT = typename Last<xs...>::type;
+
+
+template <typename... xs>
+struct Last<TypeList<xs...>> : Last<xs...>
+{ };
+
+template <typename... xs>
+struct Last<std::tuple<xs...>> : Last<xs...>
+{ };
 
 
 #endif //ROCKY_SKULL_LAST_H
--- rocky/test/skull/LastTest.cpp
+++ rocky/test/skull/LastTest.cpp
@@ -3,14 +3,23 @@
 #include "rocky/skull/Last.h"
 
 
-TEST_CASE("Last", "[skull]")
+TEST_CASE("LastT", "[skull]")
 {
     using std::is_same;
+    using std::tuple;
 
     // NOTE: following is an expected compile-time error.
-    //static_assert(is_same<double, Last<>, "");
+    //static_assert(is_same<double, LastT<>, "");
+    //static_assert(is_same<double, LastT<TypeList<>>, "");
+    //static_assert(is_same<double, LastT<tuple<>>, "");
 
-    static_assert(is_same<double, Last<double>>(), "");
-    static_assert(is_same<double, Last<char, int, float, double>>(), "");
+    static_assert(is_same<double, LastT<double>>(), "");
+    static_assert(is_same<double, LastT<char, int, float, double>>(), "");
+
+    static_assert(is_same<double, LastT<TypeList<double>>>(), "");
+    static_assert(is_same<double, LastT<TypeList<char, int, float, double>>>(), "");
+
+    static_assert(is_same<double, LastT<tuple<double>>>(), "");
+    static_assert(is_same<double, LastT<tuple<char, int, float, double>>>(), "");
 }
 
