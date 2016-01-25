f5ac79e5ef1e16f9a5141629188eaa2f6469f5d2
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-25 16:03:53 +0900

    implemented Replicate.
    - added test.

--- rocky/skull/Replicate.h
+++ rocky/skull/Replicate.h
@@ -2,8 +2,36 @@
 #define ROCKY_SKULL_REPLICATE_H
 
 
-template <std::size_t n, typename T>
+#include "rocky/skull/Head.h"
+
+
+template <std::size_t n, typename x>
 struct Replicate
+{
+private:
+    template <typename IndexSequence, typename T>
+    struct ReplicateImpl;
+
+    template <std::size_t... i, typename T>
+    struct ReplicateImpl<std::index_sequence<i...>, T>
+            : type_is<
+                    TypeList<
+                            HeadT<TypeList<T, std::integral_constant<std::size_t, i>>>...
+                    >
+              >
+    { };
+
+public:
+    using type = typename ReplicateImpl<std::make_index_sequence<n>, x>::type;
+};
+
+
+template <std::size_t n, typename x>
+using ReplicateT = typename Replicate<n, x>::type;
+
+
+template <typename x>
+struct Replicate<0, x> : type_is<TypeList<>>
 { };
 
 
--- rocky/test/skull/ReplicateTest.cpp
+++ rocky/test/skull/ReplicateTest.cpp
@@ -2,9 +2,31 @@
 
 #include "rocky/skull/Replicate.h"
 
+#include "rocky/meta/IntegralConstantUtility.h"
+
 
 TEST_CASE("Replicate", "[skull]")
 {
-    
+    using std::is_same;
+    using std::tuple;
+
+    static_assert(is_same<TypeList<>, ReplicateT<0, char>>(), "");
+
+    static_assert(
+            is_same<
+                    TypeList<int_c_t<0>, int_c_t<0>, int_c_t<0>>,
+                    ReplicateT<3, int_c_t<0>>
+            >(),
+            ""
+    );
+
+    static_assert(is_same<TypeList<char, char, char>, ReplicateT<3, char>>(), "");
+    static_assert(
+            is_same<
+                    TypeList<TypeList<char>, TypeList<char>, TypeList<char>>,
+                    ReplicateT<3, TypeList<char>>
+            >(),
+            ""
+    );
 }
 
