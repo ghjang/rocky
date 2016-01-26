3c839ad19705a2338629a5c75635f3afe66d6e68
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-26 13:25:33 +0900

    added Any metafunction and test.

---
+++ rocky/skull/Any.h
@@ -0,0 +1,23 @@
+#ifndef ROCKY_SKULL_ANY_H
+#define ROCKY_SKULL_ANY_H
+
+
+#include "rocky/meta/FindIf.h"
+
+
+template <template <typename> class p, typename... xs>
+struct Any : std::integral_constant<bool, FindIf<p, xs...>() != -1>
+{ };
+
+
+template <template <typename> class p, typename... xs>
+struct Any<p, TypeList<xs...>> : Any<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct Any<p, std::tuple<xs...>> : Any<p, xs...>
+{ };
+
+
+#endif //ROCKY_SKULL_ANY_H
+
---
+++ rocky/test/skull/AnyTest.cpp
@@ -0,0 +1,24 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/Any.h"
+
+
+TEST_CASE("Any", "[skull]")
+{
+    using std::is_integral;
+    using std::is_floating_point;
+    using std::tuple;
+
+    static_assert(!Any<is_integral>(), "");
+    static_assert(!Any<is_integral, TypeList<>>(), "");
+    static_assert(!Any<is_integral, tuple<>>(), "");
+
+    static_assert(Any<is_integral, float, int, double>(), "");
+    static_assert(Any<is_integral, TypeList<float, int, double>>(), "");
+    static_assert(Any<is_integral, tuple<float, int, double>>(), "");
+
+    static_assert(!Any<is_floating_point, char, int, long>(), "");
+    static_assert(!Any<is_floating_point, TypeList<char, int, long>>(), "");
+    static_assert(!Any<is_floating_point, tuple<char, int, long>>(), "");
+}
+
