8252defa03e9d8b48c97d4f7deb3bd7e25f67b55
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-27 02:22:33 +0900

    added Divisor metafunction and test.

---
+++ rocky/math/Divisor.h
@@ -0,0 +1,54 @@
+#ifndef ROCKY_MATH_DIVISOR_H
+#define ROCKY_MATH_DIVISOR_H
+
+
+#include "rocky/base/TypeList.h"
+#include "rocky/base/IntegralConstantUtility.h"
+#include "rocky/math/Abs.h"
+
+
+namespace Detail
+{
+    template <typename... T>
+    struct DivisorImpl;
+
+    template <typename... xs, typename T, T i>
+    struct DivisorImpl<TypeList<xs...>, std::integral_constant<T, i>, std::integral_constant<T, i>>
+            : type_is<TypeList<xs..., std::integral_constant<T, i>>>
+    { };
+
+    template <typename... xs, typename T, T i, T j>
+    struct DivisorImpl<TypeList<xs...>, std::integral_constant<T, i>, std::integral_constant<T, j>>
+            : DivisorImpl<
+                    std::conditional_t<
+                            (i % j) == 0, // if divisor,
+                            TypeList<xs..., std::integral_constant<T, j>>,
+                            TypeList<xs...>
+                    >,
+                    std::integral_constant<T, i>,
+                    std::integral_constant<T, j + 1>
+              >
+    { };
+} // namespace Detail
+
+
+template <typename T>
+struct Divisor;
+
+template <typename T>
+struct Divisor<std::integral_constant<T, 0>>
+        : type_is<TypeList<>>
+{ };
+
+template <typename T, T i>
+struct Divisor<std::integral_constant<T, i>>
+        : Detail::DivisorImpl<TypeList<>, std::integral_constant<T, Abs(i)>, std::integral_constant<T, 1>>
+{ };
+
+
+template <typename T>
+using DivisorT = typename Divisor<T>::type;
+
+
+#endif //ROCKY_MATH_DIVISOR_H
+
---
+++ rocky/test/math/DivisorTest.cpp
@@ -0,0 +1,34 @@
+#include "../catch.hpp"
+
+#include "rocky/math/Divisor.h"
+
+
+TEST_CASE("Divisor", "[math]")
+{
+    using std::is_same;
+
+    static_assert(
+            is_same<
+                    TypeList<>,
+                    DivisorT<int_c_t<0>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<int_c_t<1>, int_c_t<2>, int_c_t<4>, int_c_t<5>, int_c_t<10>, int_c_t<20>>,
+                    DivisorT<int_c_t<20>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+            TypeList<int_c_t<1>, int_c_t<2>, int_c_t<4>, int_c_t<5>, int_c_t<10>, int_c_t<20>>,
+            DivisorT<int_c_t<-20>>
+            >(),
+            ""
+    );
+}
+
