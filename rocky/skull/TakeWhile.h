79e5b05d451f1429dcf0444455d251b7d9712871
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-26 12:35:08 +0900

    added TakeWhile metafunction and test.

---
+++ rocky/skull/TakeWhile.h
@@ -0,0 +1,45 @@
+#ifndef ROCKY_SKULL_TAKEWHILE_H
+#define ROCKY_SKULL_TAKEWHILE_H
+
+
+#include "rocky/meta/TypeListFlatten.h"
+#include "rocky/meta/TypeSelection.h"
+
+
+template <template <typename> class p, typename... xs>
+struct TakeWhile;
+
+template <template <typename> class p, typename... xs>
+using TakeWhileT = typename TakeWhile<p, xs...>::type;
+
+template <template <typename> class p>
+struct TakeWhile<p> : type_is<TypeList<>>
+{ };
+
+template <template <typename> class p, typename x, typename... xs>
+struct TakeWhile<p, x, xs...>
+            : type_is<
+                    FlattenTypeListT<
+                            SelectTypeIfT<
+                                    !p<x>(),
+                                    TypeList<>,
+                                    FlattenTypeList<x, TakeWhileT<p, xs...>>
+                            >,
+                            TypeList<>
+                    >
+              >
+{
+    static_assert(HasValueMember<p<x>>(), "Predicate 'p' should have 'value' member.");
+};
+
+template <template <typename> class p, typename... xs>
+struct TakeWhile<p, TypeList<xs...>> : TakeWhile<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct TakeWhile<p, std::tuple<xs...>> : TypeListToTuple<TakeWhileT<p, xs...>>
+{ };
+
+
+#endif //ROCKY_SKULL_TAKEWHILE_H
+
---
+++ rocky/test/skull/TakeWhileTest.cpp
@@ -0,0 +1,62 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/TakeWhile.h"
+
+#include "rocky/meta/TypeUtility.h"
+
+
+TEST_CASE("TakeWhile", "[skull]")
+{
+    using std::is_same;
+    using std::is_integral;
+    using std::tuple;
+
+    static_assert(
+            is_same<
+                    TypeList<>,
+                    TakeWhileT<is_integral>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<char, int>,
+                    TakeWhileT<is_integral, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<char, int, long, long long>,
+                    TakeWhileT<is_integral, char, int, long, long long>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<>,
+                    TakeWhileT<NegatePredicate<is_integral>::template Convert, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<char, int>,
+                    TakeWhileT<is_integral, TypeList<char, int, float, double>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    tuple<char, int>,
+                    TakeWhileT<is_integral, tuple<char, int, float, double>>
+            >(),
+            ""
+    );
+}
+
