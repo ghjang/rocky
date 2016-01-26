80049cb9fb95f31bf57aa0d42b189f7e4448729b
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-26 11:47:24 +0900

    added All metafunction and test.

---
+++ rocky/skull/All.h
@@ -0,0 +1,24 @@
+#ifndef ROCKY_SKULL_ALL_H
+#define ROCKY_SKULL_ALL_H
+
+
+#include "rocky/skull/Sum.h"
+#include "rocky/skull/Map.h"
+
+
+template <template <typename> class p, typename... xs>
+struct All : std::integral_constant<bool, SumT<MapT<p, xs...>>() == sizeof...(xs)>
+{ };
+
+
+template <template <typename> class p, typename... xs>
+struct All<p, TypeList<xs...>> : All<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct All<p, std::tuple<xs...>> : All<p, xs...>
+{ };
+
+
+#endif //ROCKY_SKULL_ALL_H
+
---
+++ rocky/test/skull/AllTest.cpp
@@ -0,0 +1,21 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/All.h"
+
+
+TEST_CASE("All", "[skull]")
+{
+    using std::is_same;
+    using std::is_pointer;
+    using std::tuple;
+
+    static_assert(All<is_pointer, char *, int *, float *, double *>(), "");
+    static_assert(!All<is_pointer, char *, int *, float, double *>(), "");
+
+    static_assert(All<is_pointer, TypeList<char *, int *, float *, double *>>(), "");
+    static_assert(!All<is_pointer, TypeList<char *, int *, float, double *>>(), "");
+
+    static_assert(All<is_pointer, tuple<char *, int *, float *, double *>>(), "");
+    static_assert(!All<is_pointer, tuple<char *, int *, float, double *>>(), "");
+}
+
