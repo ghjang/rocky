cb18a787280fe294b0ee005d889712fdd76d2c35
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-01-26 13:17:45 +0900

    added FindIf metafunction and test.

---
+++ rocky/meta/FindIf.h
@@ -0,0 +1,58 @@
+#ifndef ROCKY_FIND_IF_H
+#define ROCKY_FIND_IF_H
+
+
+#include "rocky/meta/IntegralConstantUtility.h"
+#include "rocky/skull/Reverse.h"
+
+
+template <template <typename> class p, typename... xs>
+struct FindIf : FindIf<p, int_c_t<0>, xs...>
+{ };
+
+template <template <typename> class p, int i>
+struct FindIf<p, int_c_t<i>> : std::integral_constant<int, -1>
+{ };
+
+template <template <typename> class p, int i, typename x, typename... xs>
+struct FindIf<p, int_c_t<i>, x, xs...>
+        : SelectTypeIfT<
+                p<x>::value,
+                std::integral_constant<int, i>,
+                FindIf<p, int_c_t<i + 1>, xs...>
+          >
+{ };
+
+
+template <template <typename> class p, typename... xs>
+struct FindIf<p, TypeList<xs...>> : FindIf<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct FindIf<p, std::tuple<xs...>> : FindIf<p, xs...>
+{ };
+
+
+template <template <typename> class p, typename... xs>
+struct ReverseFindIf
+{
+private:
+    static constexpr int i = FindIf<p, ReverseT<xs...>>();
+
+public:
+    static constexpr int value = (i == -1) ? (-1) : (sizeof...(xs) - i - 1);
+
+    constexpr operator int () const noexcept { return value; }
+};
+
+template <template <typename> class p, typename... xs>
+struct ReverseFindIf<p, TypeList<xs...>> : ReverseFindIf<p, xs...>
+{ };
+
+template <template <typename> class p, typename... xs>
+struct ReverseFindIf<p, std::tuple<xs...>> : ReverseFindIf<p, xs...>
+{ };
+
+
+#endif //ROCKY_FIND_IF_H
+
---
+++ rocky/test/meta/FindIfTest.cpp
@@ -0,0 +1,23 @@
+#include "../catch.hpp"
+
+#include "rocky/meta/FindIf.h"
+
+
+TEST_CASE("FindIf", "[skull]")
+{
+    using std::is_floating_point;
+    using std::tuple;
+
+    static_assert(-1 == FindIf<is_floating_point>(), "");
+    static_assert(-1 == FindIf<is_floating_point, TypeList<>>(), "");
+    static_assert(-1 == FindIf<is_floating_point, tuple<>>(), "");
+
+    static_assert(-1 == FindIf<is_floating_point, char, int>(), "");
+    static_assert(-1 == FindIf<is_floating_point, TypeList<char, int>>(), "");
+    static_assert(-1 == FindIf<is_floating_point, tuple<char, int>>(), "");
+
+    static_assert(2 == FindIf<is_floating_point, char, int, float, double>(), "");
+    static_assert(2 == FindIf<is_floating_point, TypeList<char, int, float, double>>(), "");
+    static_assert(2 == FindIf<is_floating_point, tuple<char, int, float, double>>(), "");
+}
+
