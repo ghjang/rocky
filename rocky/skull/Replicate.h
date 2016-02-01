b57cb6b5701fb7e843edb39ea4a58f630183ca17
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-02 00:14:57 +0900

    modified interface.

--- rocky/skull/Replicate.h
+++ rocky/skull/Replicate.h
@@ -5,18 +5,21 @@
 #include "rocky/skull/Head.h"
 
 
-template <std::size_t n, typename x>
-struct Replicate
+template <typename n, typename x>
+struct Replicate;
+
+template <typename T, T n, typename x>
+struct Replicate<std::integral_constant<T, n>, x>
 {
 private:
-    template <typename IndexSequence, typename T>
+    template <typename IndexSequence, typename T1>
     struct ReplicateImpl;
 
-    template <std::size_t... i, typename T>
-    struct ReplicateImpl<std::index_sequence<i...>, T>
+    template <std::size_t... i, typename T1>
+    struct ReplicateImpl<std::index_sequence<i...>, T1>
             : type_is<
                     TypeList<
-                            HeadT<TypeList<T, std::integral_constant<std::size_t, i>>>...
+                            HeadT<TypeList<T1, std::integral_constant<std::size_t, i>>>...
                     >
               >
     { };
@@ -27,11 +30,11 @@ public:
 
 
 template <std::size_t n, typename x>
-using ReplicateT = typename Replicate<n, x>::type;
+using ReplicateT = typename Replicate<std::integral_constant<std::size_t, n>, x>::type;
 
 
 template <typename x>
-struct Replicate<0, x> : type_is<TypeList<>>
+struct Replicate<std::integral_constant<std::size_t, 0>, x> : type_is<TypeList<>>
 { };
 
 
