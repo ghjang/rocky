5a93c250aaee2f37908a7aa8328fc411dd9b562e
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-01 15:34:02 +0900

    added Partition metafunction and test.

---
+++ rocky/test/wolca/PartitionTest.cpp
@@ -0,0 +1,70 @@
+#include "../catch.hpp"
+
+#include "rocky/wolca/Partition.h"
+
+#include "rocky/base/IntegralConstantUtility.h"
+
+
+TEST_CASE("Partition", "[wolca]")
+{
+    using std::is_same;
+
+    // NOTE: exptected compile-time errors.
+    /*
+    static_assert(
+            is_same<
+                    TL<TL<char, int>, TL<float, double>>,
+                    PartitionT<int_c_t<-1>, char, int, float, double>
+            >(),
+            ""
+    );
+    static_assert(
+            is_same<
+                    TL<TL<char, int>, TL<float, double>>,
+                    PartitionT<int_c_t<0>, char, int, float, double>
+            >(),
+            ""
+    );
+     */
+
+    static_assert(
+            is_same<
+                    TL<TL<char>, TL<int>, TL<float>, TL<double>>,
+                    PartitionT<int_c_t<1>, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TL<TL<char, int>, TL<float, double>>,
+                    PartitionT<int_c_t<2>, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TL<TL<char, int, float>>,
+                    PartitionT<int_c_t<3>, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TL<TL<char, int, float, double>>,
+                    PartitionT<int_c_t<4>, char, int, float, double>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TL<>,
+                    PartitionT<int_c_t<5>, char, int, float, double>
+            >(),
+            ""
+    );
+}
+
---
+++ rocky/wolca/Partition.h
@@ -0,0 +1,55 @@
+#ifndef ROCKY_WOLCA_PARTITION_H
+#define ROCKY_WOLCA_PARTITION_H
+
+
+#include "rocky/skull/Take.h"
+#include "rocky/skull/Drop.h"
+
+
+namespace Detail
+{
+    template <typename T, typename... xs>
+    struct PartitionImpl;
+
+    template <typename T, T n, typename... rs, typename... xs>
+    struct PartitionImpl<std::integral_constant<T, n>, TypeList<rs...>, TypeList<xs...>>
+            : SelectTypeIf<
+                    (n > sizeof...(xs)),
+                    TypeList<rs...>,
+                    PartitionImpl<
+                            std::integral_constant<T, n>,
+                            TypeList<rs..., TakeT<n, xs...>>,
+                            DropT<n, xs...>
+                    >
+              >
+    { };
+} // namespace Detail
+
+
+template <typename n, typename... xs>
+struct Partition;
+
+
+template <typename n, typename... xs>
+using PartitionT = typename Partition<n, xs...>::type;
+
+
+template <typename T, T n, typename... xs>
+struct Partition<std::integral_constant<T, n>, xs...>
+{
+    static_assert(n > 0, "n should be greater than zero.");
+    static_assert(
+            std::is_integral<decltype(sizeof...(xs) / n)>(),
+            "the type of number of partition should be integral."
+    );
+
+    using type = SelectTypeIfT<
+                    (n > sizeof...(xs)),
+                    TypeList<>,
+                    Detail::PartitionImpl<std::integral_constant<T, n>, TypeList<TakeT<n, xs...>>, DropT<n, xs...>>
+                 >;
+};
+
+
+#endif //ROCKY_WOLCA_PARTITION_H
+
