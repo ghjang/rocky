9ccc845628243307079c28b02f10071f00c2a23c
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-03 22:03:03 +0900

    added files for musical scale calculation.
    - not implemented yet.

---
+++ rocky/muse/MusicalScale.h
@@ -0,0 +1,9 @@
+#ifndef ROCKY_MUSE_MUSICALSCALE_H
+#define ROCKY_MUSE_MUSICALSCALE_H
+
+
+#include "rocky/muse/MusicalNote.h"
+
+
+#endif //ROCKY_MUSE_MUSICALSCALE_H
+
---
+++ rocky/test/muse/MusicalScaleTest.cpp
@@ -0,0 +1,10 @@
+#include "../catch.hpp"
+
+#include "rocky/muse/MusicalScale.h"
+
+
+TEST_CASE("MusicalScale", "[muse]")
+{
+    
+}
+
