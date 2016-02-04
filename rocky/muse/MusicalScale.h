8fc4118ab63f857bdf071471469224681576fd6a
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-04 23:36:23 +0900

    made code more clear.

--- rocky/muse/MusicalScale.h
+++ rocky/muse/MusicalScale.h
@@ -14,27 +14,31 @@ namespace Detail
     template <typename NoteList>
     struct RotateLeftNote;
 
-    template <typename... Note>
-    struct RotateLeftNote<TypeList<Note...>>
-            : Rotate<std::integral_constant<int, -1>, TypeList<Note...>>
+    template <MusicalNote... Note>
+    struct RotateLeftNote<TypeList<musical_note_c_t<Note>...>>
+            : Rotate<std::integral_constant<int, -1>, TypeList<musical_note_c_t<Note>...>>
     { };
 
 
     using MajorScaleNoteIndexSequenceT = std::index_sequence<0, 2, 4, 5, 7, 9, 11>;
 
 
-    template <typename NoteList>
-    struct ExtractNote;
+    template <typename NoteIndexSequence>
+    struct ExtractNote
+    {
+        template <typename NoteList>
+        struct DoExtract;
 
-    template <MusicalNote... Note>
-    struct ExtractNote<TypeList<musical_note_c_t<Note>...>>
-            : Extract<MajorScaleNoteIndexSequenceT, TypeList<musical_note_c_t<Note>...>>
-    { };
+        template <MusicalNote... Note>
+        struct DoExtract<TypeList<musical_note_c_t<Note>...>>
+                : Extract<NoteIndexSequence, TypeList<musical_note_c_t<Note>...>>
+        { };
+    };
 } // namespace Detail
 
 
 using TwelveMajorScaleListT = MapT<
-                                    Detail::ExtractNote,
+                                    Detail::ExtractNote<Detail::MajorScaleNoteIndexSequenceT>::template DoExtract,
                                     NestListT<Detail::RotateLeftNote, TwelveHalfMusicalNoteList, 11>
                               >;
 
