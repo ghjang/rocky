86cec6060aa4ef70fa551664e7e6d79591b66489
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-05 22:25:41 +0900

    added Concat metafunction and test.
    - NOTE: It is similar to FlattenAsTypeList. Refer to FlattenAsTypeList.

---
+++ rocky/skull/Concat.h
@@ -0,0 +1,33 @@
+#ifndef ROCKY_SKULL_CONCAT_H
+#define ROCKY_SKULL_CONCAT_H
+
+
+#include "rocky/skull/FoldL.h"
+
+
+template <typename xs, typename... xss>
+struct Concat
+{
+private:
+    template <typename lhs, typename rhs>
+    struct ConcatImpl;
+
+    template <typename... lhsXs, typename... rhsYs>
+    struct ConcatImpl<TypeList<lhsXs...>, TypeList<rhsYs...>> : type_is<TypeList<lhsXs..., rhsYs...>>
+    { };
+
+    template <typename... lhsXs, typename... rhsYs>
+    struct ConcatImpl<std::tuple<lhsXs...>, std::tuple<rhsYs...>> : type_is<std::tuple<lhsXs..., rhsYs...>>
+    { };
+
+public:
+    using type = FoldLT<ConcatImpl, xs, xss...>;
+};
+
+
+template <typename... xss>
+using ConcatT = typename Concat<xss...>::type;
+
+
+#endif //ROCKY_SKULL_CONCAT_H
+
---
+++ rocky/test/skull/ConcatTest.cpp
@@ -0,0 +1,44 @@
+#include "../catch.hpp"
+
+#include "rocky/skull/Concat.h"
+
+
+TEST_CASE("Concat", "[skull]")
+{
+    using std::is_same;
+    using std::tuple;
+
+    // NOTE: following is an expected compile-time error.
+    //static_assert(is_same<TL<>, ConcatT<>>(), "");
+
+    static_assert(is_same<TL<>, ConcatT<TL<>>>(), "");
+    static_assert(is_same<TL<>, ConcatT<TL<>, TL<>>>(), "");
+
+    static_assert(is_same<TL<char, int, float, double>, ConcatT<TL<char, int>, TL<float, double>>>(), "");
+    static_assert(is_same<TL<char, int, float, double>, ConcatT<TL<char, int>, TL<>, TL<float, double>>>(), "");
+
+    static_assert(
+            is_same<
+                    tuple<char, int, float, double>,
+                    ConcatT<tuple<char, int>, tuple<>, tuple<float, double>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TL<TL<char, int>, TL<>, TL<float, double>>,
+                    ConcatT<TL<TL<char, int>>, TL<TL<>>, TL<TL<float, double>>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    tuple<tuple<char, int>, tuple<>, tuple<float, double>>,
+                    ConcatT<tuple<tuple<char, int>>, tuple<tuple<>>, tuple<tuple<float, double>>>
+            >(),
+            ""
+    );
+}
+
