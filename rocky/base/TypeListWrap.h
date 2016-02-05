c18ba32b91293ea7eb38c2d25c5e69b77fad5ed0
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-05 21:40:18 +0900

    changed metafunction name from JoinTypeList to WrapAsTypeList.

--- rocky/base/TypeListWrap.h
+++ rocky/base/TypeListWrap.h
@@ -1,31 +1,31 @@
-#ifndef ROCKY_BASE_TYPELISTJOIN_H
-#define ROCKY_BASE_TYPELISTJOIN_H
+#ifndef ROCKY_BASE_TYPELISTWRAP_H
+#define ROCKY_BASE_TYPELISTWRAP_H
 
 
 #include "rocky/base/TypeList.h"
 
 
 template <typename... xs>
-struct JoinTypeList;
+struct WrapAsTypeList;
 
 template <typename... T1, typename... T2>
-struct JoinTypeList<TypeList<T1...>, TypeList<T2...>>
+struct WrapAsTypeList<TypeList<T1...>, TypeList<T2...>>
         : type_is<TypeList<TypeList<T1...>, TypeList<T2...>>>
 { };
 
 template <typename... T1, typename... T2>
-struct JoinTypeList<TypeList<T1...>, T2...>
+struct WrapAsTypeList<TypeList<T1...>, T2...>
         : type_is<TypeList<TypeList<T1...>, T2...>>
 { };
 
 template <typename T1, typename... T2>
-struct JoinTypeList<T1, TypeList<T2...>>
+struct WrapAsTypeList<T1, TypeList<T2...>>
         : type_is<TypeList<T1, TypeList<T2...>>>
 { };
 
 
 template <typename... xs>
-using JoinTypeListT = typename JoinTypeList<xs...>::type;
+using WrapAsTypeListT = typename WrapAsTypeList<xs...>::type;
 
 
 template <typename... T1, typename... T2>
@@ -35,5 +35,5 @@ constexpr auto operator + (TypeList<T1...>, TypeList<T2...>)
 }
 
 
-#endif //ROCKY_BASE_TYPELISTJOIN_H
+#endif //ROCKY_BASE_TYPELISTWRAP_H
 
--- rocky/test/base/TypeListWrapTest.cpp
+++ rocky/test/base/TypeListWrapTest.cpp
@@ -3,25 +3,25 @@
 #include "rocky/base/TypeListWrap.h"
 
 
-TEST_CASE("TypeListJoin", "[TypeListJoin]")
+TEST_CASE("WarpAsTypeList", "[WrapAsTypeList]")
 {
     using std::is_same;
     using std::tuple;
 
-    static_assert(is_same<TypeList<TypeList<>, TypeList<>>, JoinTypeListT<TypeList<>, TypeList<>>>(), "");
-    static_assert(is_same<TypeList<TypeList<char>, TypeList<>>, JoinTypeListT<TypeList<char>, TypeList<>>>(), "");
-    static_assert(is_same<TypeList<TypeList<>, TypeList<char>>, JoinTypeListT<TypeList<>, TypeList<char>>>(), "");
+    static_assert(is_same<TL<TL<>, TL<>>, WrapAsTypeListT<TL<>, TL<>>>(), "");
+    static_assert(is_same<TL<TL<char>, TL<>>, WrapAsTypeListT<TL<char>, TL<>>>(), "");
+    static_assert(is_same<TL<TL<>, TL<char>>, WrapAsTypeListT<TL<>, TL<char>>>(), "");
     static_assert(
-            is_same<TypeList<TypeList<char>, TypeList<char>>, JoinTypeListT<TypeList<char>, TypeList<char>>>(),
+            is_same<TL<TL<char>, TL<char>>, WrapAsTypeListT<TL<char>, TL<char>>>(),
             ""
     );
 
     static_assert(
-            is_same<TypeList<TypeList<char>, char, int>, JoinTypeListT<TypeList<char>, char, int>>(),
+            is_same<TL<TL<char>, char, int>, WrapAsTypeListT<TL<char>, char, int>>(),
             ""
     );
     static_assert(
-            is_same<TypeList<char, TypeList<char, int>>, JoinTypeListT<char, TypeList<char, int>>>(),
+            is_same<TL<char, TL<char, int>>, WrapAsTypeListT<char, TL<char, int>>>(),
             ""
     );
 }
@@ -31,10 +31,10 @@ TEST_CASE("TypeList operator +", "[TypeListJoin]")
     using std::is_same;
     using std::decay_t;
 
-    constexpr auto crt = TypeList<char, int>{} + TypeList<float, double>{};
-    static_assert(is_same<decay_t<decltype(crt)>, TypeList<TypeList<char, int>, TypeList<float, double>>>(), "");
+    constexpr auto crt = TL<char, int>{} + TL<float, double>{};
+    static_assert(is_same<decay_t<decltype(crt)>, TL<TL<char, int>, TL<float, double>>>(), "");
 
-    auto rt = TypeList<char, int>{} + TypeList<float, double>{};
-    static_assert(is_same<decltype(rt), TypeList<TypeList<char, int>, TypeList<float, double>>>(), "");
+    auto rt = TL<char, int>{} + TL<float, double>{};
+    static_assert(is_same<decltype(rt), TL<TL<char, int>, TL<float, double>>>(), "");
 }
 
