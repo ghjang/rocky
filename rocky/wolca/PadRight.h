2f24b1c1ff380de763f3eca4157a4dae2df5e43c
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-05 23:58:58 +0900

    added PadRight metafunction and test.

---
+++ rocky/wolca/PadRight.h
@@ -0,0 +1,37 @@
+#ifndef ROCKY_WOLCA_PADRIGHT_H
+#define ROCKY_WOLCA_PADRIGHT_H
+
+
+#include "rocky/skull/Take.h"
+#include "rocky/skull/Replicate.h"
+#include "rocky/skull/Concat.h"
+#include "rocky/math/Distance.h"
+
+
+template <typename n, typename x, typename... xs>
+struct PadRight;
+
+template <typename T, T n, typename x, typename... xs>
+struct PadRight<std::integral_constant<T, n>, x, xs...>
+        : SelectTypeIf<
+                sizeof...(xs) >= n,
+                Take<n, xs...>,
+                Concat<
+                        TypeList<xs...>,
+                        ReplicateT<TDistanceT<n, sizeof...(xs)>::value, x>
+                >
+          >
+{ };
+
+template <typename T, T n, typename x, typename... xs>
+struct PadRight<std::integral_constant<T, n>, x, TypeList<xs...>>
+        : PadRight<std::integral_constant<T, n>, x, xs...>
+{ };
+
+
+template <std::size_t n, typename x, typename... xs>
+using PadRightT = typename PadRight<std::integral_constant<std::size_t, n>, x, xs...>::type;
+
+
+#endif //ROCKY_WOLCA_PADRIGHT_H
+
