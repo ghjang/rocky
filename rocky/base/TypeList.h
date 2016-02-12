c7a76008f555e94cdc36a798fc6150fcf09623e1
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-13 08:19:22 +0900

    added ReplaceTypeListContainerType metafunction.
    - re-implemented TypeListToTuple by using ReplaceTypeListContainerType.

--- rocky/base/TypeList.h
+++ rocky/base/TypeList.h
@@ -11,6 +11,9 @@ template <typename... xs>
 struct TypeList
 { };
 
+template <typename... xs>
+using TL = TypeList<xs...>;
+
 
 template <typename... xs>
 struct TypeListSize : std::integral_constant<std::size_t, sizeof...(xs)>
@@ -21,19 +24,24 @@ struct TypeListSize<TypeList<xs...>> : TypeListSize<xs...>
 { };
 
 
-template <typename T>
-struct TypeListToTuple;
+template <typename SourceTypeList, template <typename...> class TargetTypeListContainer>
+struct ReplaceTypeListContainerType;
 
-template <typename... xs>
-struct TypeListToTuple<TypeList<xs...>> : type_is<std::tuple<xs...>>
+template <template <typename...> class S, typename... xs, template <typename...> class T>
+struct ReplaceTypeListContainerType<S<xs...>, T> : type_is<T<xs...>>
 { };
 
+template <typename xs>
+using ToTuple = ReplaceTypeListContainerType<xs, std::tuple>;
 
-template <typename... xs>
-using TL = TypeList<xs...>;
+template <typename xs>
+using ToTupleT = typename ToTuple<xs>::type;
 
-template <typename... xs>
-using TypeListToTupleT = typename TypeListToTuple<xs...>::type;
+template <typename xs>
+using ToTypeList = typename ReplaceTypeListContainerType<xs, TypeList>::type;
+
+template <typename xs>
+using ToTypeListT = typename ToTypeList<xs>::type;
 
 
 #endif //ROCKY_BASE_TYPELIST_H
