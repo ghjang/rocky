a07591d3311f6f51fda818f0b72aec0afdb65a99
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-16 00:19:20 +0900

    added NextHalfMusicalNote metafunction and test.

--- rocky/muse/MusicalNote.h
+++ rocky/muse/MusicalNote.h
@@ -46,5 +46,23 @@ using TwelveHalfMusicalNoteList = MusicalNoteList<
                                     >;
 
 
+template <typename note>
+struct NextHalfMusicalNote;
+
+template <MusicalNote note>
+struct NextHalfMusicalNote<musical_note_c_t<note>>
+        : type_is<musical_note_c_t<static_cast<MusicalNote>(static_cast<int>(note) + 1)>>
+{ };
+
+template <>
+struct NextHalfMusicalNote<musical_note_c_t<MusicalNote::B>>
+        : type_is<musical_note_c_t<MusicalNote::C>>
+{ };
+
+
+template <MusicalNote note>
+using NextHalfMusicalNoteT = typename NextHalfMusicalNote<musical_note_c_t<note>>::type;
+
+
 #endif //ROCKY_MUSE_MUSICALNOTE_H
 
--- rocky/test/muse/MusicalNoteTest.cpp
+++ rocky/test/muse/MusicalNoteTest.cpp
@@ -5,6 +5,8 @@
 #include "rocky/skull/Head.h"
 #include "rocky/skull/Last.h"
 
+#include "rocky/wolca/Nest.h"
+
 
 TEST_CASE("MusicalNote", "[muse]")
 {
@@ -12,5 +14,49 @@ TEST_CASE("MusicalNote", "[muse]")
 
     static_assert(is_same<musical_note_c_t<MusicalNote::C>, HeadT<TwelveHalfMusicalNoteList>>(), "");
     static_assert(is_same<musical_note_c_t<MusicalNote::B>, LastT<TwelveHalfMusicalNoteList>>(), "");
+}
+
+TEST_CASE("NextHalfMusicalNote", "[muse]")
+{
+    using std::is_same;
+
+    static_assert(is_same<musical_note_c_t<MusicalNote::Db>, NextHalfMusicalNoteT<MusicalNote::C>>(), "");
+    static_assert(is_same<musical_note_c_t<MusicalNote::C>, NextHalfMusicalNoteT<MusicalNote::B>>(), "");
+
+    // perfect 4th
+    static_assert(
+            is_same<
+                    musical_note_c_t<MusicalNote::F>,
+                    NestT<Quote<NextHalfMusicalNote>, musical_note_c_t<MusicalNote::C>, 5>
+            >(),
+            ""
+    );
+
+    // perfect 5th
+    static_assert(
+            is_same<
+                    musical_note_c_t<MusicalNote::G>,
+                    NestT<Quote<NextHalfMusicalNote>, musical_note_c_t<MusicalNote::C>, 7>
+            >(),
+            ""
+    );
+
+    // 1-octave above
+    static_assert(
+            is_same<
+                    musical_note_c_t<MusicalNote::C>,
+                    NestT<Quote<NextHalfMusicalNote>, musical_note_c_t<MusicalNote::C>, 12>
+            >(),
+            ""
+    );
+
+    // 2-octave above
+    static_assert(
+            is_same<
+                    musical_note_c_t<MusicalNote::C>,
+                    NestT<Quote<NextHalfMusicalNote>, musical_note_c_t<MusicalNote::C>, 24>
+            >(),
+            ""
+    );
 }
 
