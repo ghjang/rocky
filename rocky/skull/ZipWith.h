f40df1a505633ce0b3e1a0b4e56256658e070041
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-18 04:47:51 +0900

    applied metafunction class concept.
    - removed code duplication.

--- rocky/skull/ZipWith.h
+++ rocky/skull/ZipWith.h
@@ -2,72 +2,56 @@
 #define ROCKY_SKULL_ZIPWITH_H
 
 
-#include "rocky/base/TypeList.h"
+#include "rocky/base/TypeUtility.h"
+#include "rocky/base/TypeComposition.h"
+#include "rocky/skull/Map.h"
 
 
 namespace Detail
 {
-    template <template <typename, typename> class f, typename xs, typename ys, typename zs>
+    template <typename f, typename xs, typename ys, typename zs>
     struct ZipWithImpl;
 
-
-    template <template <typename, typename> class f, typename... zs>
+    template <typename f, typename... zs>
     struct ZipWithImpl<f, TypeList<>, TypeList<>, TypeList<zs...>> : type_is<TypeList<zs...>>
     { };
 
-    template <template <typename, typename> class f, typename... xs, typename... zs>
+    template <typename f, typename... xs, typename... zs>
     struct ZipWithImpl<f, TypeList<xs...>, TypeList<>, TypeList<zs...>> : type_is<TypeList<zs...>>
     { };
 
-    template <template <typename, typename> class f, typename... ys, typename... zs>
+    template <typename f, typename... ys, typename... zs>
     struct ZipWithImpl<f, TypeList<>, TypeList<ys...>, TypeList<zs...>> : type_is<TypeList<zs...>>
     { };
 
-    template <template <typename, typename> class f, typename x, typename... xs, typename y, typename... ys, typename... zs>
+    template <typename f, typename x, typename... xs, typename y, typename... ys, typename... zs>
     struct ZipWithImpl<f, TypeList<x, xs...>, TypeList<y, ys...>, TypeList<zs...>>
-            : ZipWithImpl<f, TypeList<xs...>, TypeList<ys...>, TypeList<zs..., typename f<x, y>::type>>
-    { };
-
-
-    template <template <typename, typename> class f, typename xs, typename ys, typename zs>
-    using ZipWithImplT = typename ZipWithImpl<f, xs, ys, zs>::type;
-
-
-    template <template <typename, typename> class f, typename... zs>
-    struct ZipWithImpl<f, std::tuple<>, std::tuple<>, std::tuple<zs...>> : type_is<std::tuple<zs...>>
-    { };
-
-    template <template <typename, typename> class f, typename... xs, typename... zs>
-    struct ZipWithImpl<f, std::tuple<xs...>, std::tuple<>, std::tuple<zs...>> : type_is<std::tuple<zs...>>
-    { };
-
-    template <template <typename, typename> class f, typename... ys, typename... zs>
-    struct ZipWithImpl<f, std::tuple<>, std::tuple<ys...>, std::tuple<zs...>> : type_is<std::tuple<zs...>>
-    { };
-
-    template <template <typename, typename> class f, typename x, typename... xs, typename y, typename... ys, typename... zs>
-    struct ZipWithImpl<f, std::tuple<x, xs...>, std::tuple<y, ys...>, std::tuple<zs...>>
-            : ZipWithImpl<f, std::tuple<xs...>, std::tuple<ys...>, std::tuple<zs..., typename f<x, y>::type>>
+            : ZipWithImpl<f, TypeList<xs...>, TypeList<ys...>, TypeList<zs..., ApplyT<f, x, y>>>
     { };
 } // namespace Detail
 
 
-template <template <typename, typename> class f, typename xs, typename ys>
+/**
+ * @tparam f (binary) metafunction class
+ * @tparam xs first type list
+ * @tparam ys second type list
+ */
+template <typename f, typename xs, typename ys>
 struct ZipWith;
 
-template <template <typename, typename> class f, typename... xs, typename... ys>
+
+template <typename f, typename xs, typename ys>
+using ZipWithT = typename ZipWith<f, xs, ys>::type;
+
+
+template <typename f, typename... xs, typename... ys>
 struct ZipWith<f, TypeList<xs...>, TypeList<ys...>>
         : Detail::ZipWithImpl<f, TypeList<xs...>, TypeList<ys...>, TypeList<>>
 { };
 
-
-template <template <typename, typename> class f, typename xs, typename ys>
-using ZipWithT = typename ZipWith<f, xs, ys>::type;
-
-
-template <template <typename, typename> class f, typename... xs, typename... ys>
+template <typename f, typename... xs, typename... ys>
 struct ZipWith<f, std::tuple<xs...>, std::tuple<ys...>>
-        : Detail::ZipWithImpl<f, std::tuple<xs...>, std::tuple<ys...>, std::tuple<>>
+        : ToTuple<ZipWithT<f, TypeList<xs...>, TypeList<ys...>>>
 { };
 
 
