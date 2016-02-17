18f567bc6bbe9eedd875cbb04e5bc90bb5d5b2a6
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-18 04:48:28 +0900

    applied metafunction class concept.

--- rocky/skull/Zip.h
+++ rocky/skull/Zip.h
@@ -5,31 +5,30 @@
 #include "rocky/skull/ZipWith.h"
 
 
-namespace Detail
-{
-    template <typename x, typename y>
-    struct AsPairTypeList : type_is<TypeList<x, y>>
-    { };
-
-    template <typename x, typename y>
-    struct AsPairTuple : type_is<std::tuple<x, y>>
-    { };
-} // namespace Detail
-
-
 template <typename xs, typename ys>
 struct Zip;
 
+template <typename... xs, typename... ys>
+struct Zip<TypeList<xs...>, TypeList<ys...>>
+        : ZipWith<
+                Quote<AsPairTypeList>,
+                TypeList<xs...>,
+                TypeList<ys...>
+          >
+{ };
+
+template <typename... xs, typename... ys>
+struct Zip<std::tuple<xs...>, std::tuple<ys...>>
+        : ZipWith<
+                Quote<AsPairTuple>,
+                std::tuple<xs...>,
+                std::tuple<ys...>
+          >
+{ };
+
+
 template <typename xs, typename ys>
 using ZipT = typename Zip<xs, ys>::type;
-
-template <typename... xs, typename... ys>
-struct Zip<TypeList<xs...>, TypeList<ys...>> : ZipWith<Detail::AsPairTypeList, TypeList<xs...>, TypeList<ys...>>
-{ };
-
-template <typename... xs, typename... ys>
-struct Zip<std::tuple<xs...>, std::tuple<ys...>> : ZipWith<Detail::AsPairTuple, std::tuple<xs...>, std::tuple<ys...>>
-{ };
 
 
 #endif //ROCKY_SKULL_ZIP_H
