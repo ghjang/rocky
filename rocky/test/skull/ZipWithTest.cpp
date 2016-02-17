288b361ad0ca3f8689cb8805fbd149d7c85f9f49
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-18 04:49:20 +0900

    added a test case.
    - updated test cases to apply the modified interfaces.

--- rocky/test/skull/ZipWithTest.cpp
+++ rocky/test/skull/ZipWithTest.cpp
@@ -6,7 +6,52 @@
 #include "rocky/skull/Replicate.h"
 #include "rocky/skull/Range.h"
 #include "rocky/skull/Max.h"
+#include "rocky/skull/Map.h"
 
+
+TEST_CASE("ZipWith", "[skull]")
+{
+    using std::is_same;
+    using std::tuple;
+
+    static_assert(
+            is_same<
+                    TypeList<>,
+                    MapT<
+                            Quote<ToTuple>,
+                            ZipWithT<Quote<AsPairTuple>, TypeList<>, TypeList<>>
+                    >
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    TypeList<tuple<char, int>, tuple<char, int>>,
+                    MapT<
+                            Quote<ToTuple>,
+                            ZipWithT<Quote<AsPairTuple>, TypeList<char, char>, TypeList<int, int>>
+                    >
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    tuple<tuple<char, int>, tuple<char, int>>,
+                    ToTupleT<TypeList<tuple<char, int>, tuple<char, int>>>
+            >(),
+            ""
+    );
+
+    static_assert(
+            is_same<
+                    tuple<tuple<char, int>, tuple<char, int>>,
+                    ToTupleT<ZipWithT<Quote<AsPairTuple>, TypeList<char, char>, TypeList<int, int>>>
+            >(),
+            ""
+    );
+}
 
 TEST_CASE("ZipWith Plus", "[skull]")
 {
@@ -16,7 +61,7 @@ TEST_CASE("ZipWith Plus", "[skull]")
             is_same<
                     TL<int_c_t<2>, int_c_t<4>, int_c_t<6>>,
                     ZipWithT<
-                            Plus,
+                            Quote<Plus>,
                             TL<int_c_t<1>, int_c_t<2>, int_c_t<3>>,
                             TL<int_c_t<1>, int_c_t<2>, int_c_t<3>>
                     >
@@ -33,7 +78,7 @@ TEST_CASE("ZipWith Max", "[skull]")
             is_same<
                     TL<int_c_t<6>, int_c_t<7>, int_c_t<8>>,
                     ZipWithT<
-                            Max,
+                            Quote<Max>,
                             TL<int_c_t<4>, int_c_t<7>, int_c_t<8>>,
                             TL<int_c_t<6>, int_c_t<1>, int_c_t<7>>
                     >
@@ -50,7 +95,7 @@ TEST_CASE("ZipWith Multiply", "[skull]")
             is_same<
                     TL<int_c_t<2>, int_c_t<4>, int_c_t<6>, int_c_t<8>, int_c_t<10>>,
                     ZipWithT<
-                            Multiply,
+                            Quote<Multiply>,
                             ReplicateT<5, int_c_t<2>>,
                             RangeT<int_c_t<1>, int_c_t<5>>
                     >
