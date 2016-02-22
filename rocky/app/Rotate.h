9fff25a99b4e568b25177359d42a60d1fdc37b8f
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-23 04:39:08 +0900

    made it more generic and consistent.

--- rocky/app/Rotate.h
+++ rocky/app/Rotate.h
@@ -9,30 +9,38 @@
 #include "rocky/skull/Drop.h"
 
 
+namespace Detail
+{
+    template <typename n, typename... xs>
+    struct RotateImpl;
+
+    template <typename T, T n>
+    struct RotateImpl<std::integral_constant<T, n>> : type_is<TypeList<>>
+    { };
+
+    template <typename T, typename x, typename... xs>
+    struct RotateImpl<std::integral_constant<T, 0>, x, xs...> : type_is<TypeList<x, xs...>>
+    { };
+
+    template <typename T, T i, typename... xs>
+    struct RotateImpl<std::integral_constant<T, i>, xs...>
+            : SelectTypeIf<
+                    i < 0,
+                    FlattenAsTypeList<
+                            DropT<Abs(i) % sizeof...(xs), xs...>,
+                            TakeT<Abs(i) % sizeof...(xs), xs...>
+                    >, // left-rotation
+                    FlattenAsTypeList<
+                            DropT<sizeof...(xs) - (i % sizeof...(xs)), xs...>,
+                            TakeT<sizeof...(xs) - (i % sizeof...(xs)), xs...>
+                    > // right-rotation
+              >
+    { };
+} // namespace Detail
+
+
 template <typename n, typename... xs>
-struct Rotate;
-
-template <typename T, T n>
-struct Rotate<std::integral_constant<T, n>> : type_is<TypeList<>>
-{ };
-
-template <typename T, typename x, typename... xs>
-struct Rotate<std::integral_constant<T, 0>, x, xs...> : type_is<TypeList<x, xs...>>
-{ };
-
-template <typename T, T i, typename... xs>
-struct Rotate<std::integral_constant<T, i>, xs...>
-        : SelectTypeIf<
-                i < 0,
-                FlattenAsTypeList<
-                        DropT<Abs(i) % sizeof...(xs), xs...>,
-                        TakeT<Abs(i) % sizeof...(xs), xs...>
-                >, // left-rotation
-                FlattenAsTypeList<
-                        DropT<sizeof...(xs) - (i % sizeof...(xs)), xs...>,
-                        TakeT<sizeof...(xs) - (i % sizeof...(xs)), xs...>
-                > // right-rotation
-          >
+struct Rotate : Detail::RotateImpl<n, xs...>
 { };
 
 
@@ -40,22 +48,13 @@ template <typename n, typename... xs>
 using RotateT = typename Rotate<n, xs...>::type;
 
 
-template <typename n, typename xs>
-struct RotateWithTypeListUnpack;
-
-template <typename T, T n, typename... xs>
-struct RotateWithTypeListUnpack<std::integral_constant<T, n>, TypeList<xs...>>
-        : Rotate<std::integral_constant<T, n>, xs...>
+template <typename T, T n, template <typename...> class TypeListContainer, typename... xs>
+struct Rotate<std::integral_constant<T, n>, TypeListContainer<xs...>>
+        : ReplaceTypeListContainer<
+                RotateT<std::integral_constant<T, n>, xs...>,
+                TypeListContainer
+          >
 { };
-
-template <typename T, T n, typename... xs>
-struct RotateWithTypeListUnpack<std::integral_constant<T, n>, std::tuple<xs...>>
-        : ToTuple<RotateT<std::integral_constant<T, n>, xs...>>
-{ };
-
-
-template <typename n, typename xs>
-using RotateWithTypeListUnpackT = typename RotateWithTypeListUnpack<n, xs>::type;
 
 
 #endif //ROCKY_APP_ROTATE_H
