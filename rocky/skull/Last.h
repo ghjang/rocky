81d7404102a04a876692372bd938c24ed67afd25
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-23 02:44:52 +0900

    made it more generic.

--- rocky/skull/Last.h
+++ rocky/skull/Last.h
@@ -11,18 +11,13 @@ template <typename... xs>
 struct Last : Head<DropT<(sizeof...(xs) <= 0) ? 0 : (sizeof...(xs) - 1), xs...>>
 { };
 
+template <template <typename...> class TypeListContainer, typename... xs>
+struct Last<TypeListContainer<xs...>> : Last<xs...>
+{ };
+
 
 template <typename... xs>
 using LastT = typename Last<xs...>::type;
-
-
-template <typename... xs>
-struct Last<TypeList<xs...>> : Last<xs...>
-{ };
-
-template <typename... xs>
-struct Last<std::tuple<xs...>> : Last<xs...>
-{ };
 
 
 #endif //ROCKY_SKULL_LAST_H
