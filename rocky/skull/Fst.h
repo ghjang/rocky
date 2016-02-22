ff8b5e2751df1ac97c5de00c7aeea54afb442e15
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-23 02:36:56 +0900

    made it more generic.

--- rocky/skull/Fst.h
+++ rocky/skull/Fst.h
@@ -11,22 +11,13 @@ struct Fst : Head<xs...>
     static_assert(sizeof...(xs) == 2, "xs should be a type list of which length is 2.");
 };
 
+template <template <typename...> class TypeListContainer, typename x, typename y>
+struct Fst<TypeListContainer<x, y>> : Fst<x, y>
+{ };
+
 
 template <typename... xs>
 using FstT = typename Fst<xs...>::type;
-
-
-template <typename T1, typename T2>
-struct Fst<TypeList<T1, T2>> : Fst<T1, T2>
-{ };
-
-template <typename T1, typename T2>
-struct Fst<std::pair<T1, T2>> : Fst<T1, T2>
-{ };
-
-template <typename T1, typename T2>
-struct Fst<std::tuple<T1, T2>> : Fst<T1, T2>
-{ };
 
 
 #endif //ROCKY_SKULL_FST_H
