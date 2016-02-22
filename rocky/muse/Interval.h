6432336eb80f71454a24affc556d8f930b5c1f43
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-22 18:46:21 +0900

    added MusicalNoteInterval enum and related metafunctions.
    - added test cases.

--- rocky/muse/Interval.h
+++ rocky/muse/Interval.h
@@ -2,6 +2,7 @@
 #define ROCKY_MUSE_INTERVAL_H
 
 
+#include "rocky/wolca/Nest.h"
 #include "rocky/muse/MusicalNote.h"
 
 
@@ -39,6 +40,64 @@ struct PrevHalfMusicalNote<musical_note_c_t<MusicalNote::C>>
 
 template <MusicalNote note>
 using PrevHalfMusicalNoteT = typename PrevHalfMusicalNote<musical_note_c_t<note>>::type;
+
+
+enum struct MusicalNoteInterval : int
+{
+    Unison          = 0,
+    Minor2nd        = 1,
+    Major2nd        = 2,
+    Minor3rd        = 3,
+    Major3rd        = 4,
+    Diminished4th   = 4,
+    Perfect4th      = 5,
+    Augmented4th    = 6,
+    Diminished5th   = 6,
+    Perfect5th      = 7,
+    Augmented5th    = 8,
+    Minor6th        = 8,
+    Major6th        = 9,
+    Minor7th        = 10,
+    Major7th        = 11,
+    Octave          = 12
+};
+
+
+namespace Detail
+{
+    /**
+     * @tparam f metafunction class to generate next musical note
+     */
+    template<typename f, typename note, MusicalNoteInterval interval>
+    struct NextMusicalNote;
+
+    template<typename f, MusicalNote note, MusicalNoteInterval interval>
+    struct NextMusicalNote<f, musical_note_c_t<note>, interval>
+            : Nest<
+                    f,
+                    musical_note_c_t<note>,
+                    std::integral_constant<
+                            std::underlying_type_t<MusicalNoteInterval>,
+                            static_cast<std::underlying_type_t<MusicalNoteInterval>>(interval)
+                    >
+            >
+    { };
+} // namespace Detail
+
+
+template <MusicalNote note, MusicalNoteInterval interval>
+using NextMusicalNoteT = typename Detail::NextMusicalNote<
+                                                Quote<NextHalfMusicalNote>,
+                                                musical_note_c_t<note>,
+                                                interval
+                                    >::type;
+
+template <MusicalNote note, MusicalNoteInterval interval>
+using PrevMusicalNoteT = typename Detail::NextMusicalNote<
+                                                Quote<PrevHalfMusicalNote>,
+                                                musical_note_c_t<note>,
+                                                interval
+                                    >::type;
 
 
 #endif //ROCKY_MUSE_INTERVAL_H
--- rocky/test/muse/IntervalTest.cpp
+++ rocky/test/muse/IntervalTest.cpp
@@ -2,8 +2,6 @@
 
 #include "rocky/muse/Interval.h"
 
-#include "rocky/wolca/Nest.h"
-
 
 TEST_CASE("NextHalfMusicalNote", "[muse]")
 {
@@ -93,8 +91,109 @@ TEST_CASE("PrevHalfMusicalNote", "[muse]")
     );
 }
 
-TEST_CASE("Interval", "[muse]")
+TEST_CASE("NextMusicalNoteT", "[muse]")
 {
+    using std::is_same;
 
+    using note_list_t_0 = TypeList<
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Unison>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor2nd>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major2nd>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor3rd>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major3rd>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Diminished4th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Perfect4th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Augmented4th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Diminished5th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Perfect5th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Augmented5th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor6th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major6th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor7th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major7th>,
+                                NextMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Octave>
+                            >;
+
+    using note_list_t_1 = TypeList<
+                                musical_note_c_t<MusicalNote::C>,
+                                musical_note_c_t<MusicalNote::Db>,
+                                musical_note_c_t<MusicalNote::D>,
+                                musical_note_c_t<MusicalNote::Eb>,
+                                musical_note_c_t<MusicalNote::E>,
+                                musical_note_c_t<MusicalNote::E>,
+                                musical_note_c_t<MusicalNote::F>,
+                                musical_note_c_t<MusicalNote::Gb>,
+                                musical_note_c_t<MusicalNote::Gb>,
+                                musical_note_c_t<MusicalNote::G>,
+                                musical_note_c_t<MusicalNote::Ab>,
+                                musical_note_c_t<MusicalNote::Ab>,
+                                musical_note_c_t<MusicalNote::A>,
+                                musical_note_c_t<MusicalNote::Bb>,
+                                musical_note_c_t<MusicalNote::B>,
+                                musical_note_c_t<MusicalNote::C>
+                            >;
+
+    static_assert(is_same<note_list_t_0, note_list_t_1>(), "");
+
+    static_assert(
+            is_same<
+                    NextMusicalNoteT<MusicalNote::B, MusicalNoteInterval::Perfect5th>,
+                    musical_note_c_t<MusicalNote::Gb>
+            >(),
+            ""
+    );
+}
+
+TEST_CASE("PrevMusicalNoteT", "[muse]")
+{
+    using std::is_same;
+
+    using note_list_t_0 = TypeList<
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Unison>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor2nd>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major2nd>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor3rd>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major3rd>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Diminished4th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Perfect4th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Augmented4th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Diminished5th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Perfect5th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Augmented5th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor6th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major6th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Minor7th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Major7th>,
+                                PrevMusicalNoteT<MusicalNote::C, MusicalNoteInterval::Octave>
+                            >;
+
+    using note_list_t_1 = TypeList<
+                                musical_note_c_t<MusicalNote::C>,
+                                musical_note_c_t<MusicalNote::B>,
+                                musical_note_c_t<MusicalNote::Bb>,
+                                musical_note_c_t<MusicalNote::A>,
+                                musical_note_c_t<MusicalNote::Ab>,
+                                musical_note_c_t<MusicalNote::Ab>,
+                                musical_note_c_t<MusicalNote::G>,
+                                musical_note_c_t<MusicalNote::Gb>,
+                                musical_note_c_t<MusicalNote::Gb>,
+                                musical_note_c_t<MusicalNote::F>,
+                                musical_note_c_t<MusicalNote::E>,
+                                musical_note_c_t<MusicalNote::E>,
+                                musical_note_c_t<MusicalNote::Eb>,
+                                musical_note_c_t<MusicalNote::D>,
+                                musical_note_c_t<MusicalNote::Db>,
+                                musical_note_c_t<MusicalNote::C>
+                            >;
+
+    static_assert(is_same<note_list_t_0, note_list_t_1>(), "");
+
+    static_assert(
+            is_same<
+                    PrevMusicalNoteT<MusicalNote::B, MusicalNoteInterval::Perfect5th>,
+                    musical_note_c_t<MusicalNote::E>
+            >(),
+            ""
+    );
 }
 
