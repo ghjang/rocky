2d24fb8f6871ae3ee4de04c929854284cf54ac89
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-02-23 16:26:07 +0900

    made it more generic.

--- rocky/base/Swap.h
+++ rocky/base/Swap.h
@@ -11,24 +11,18 @@ struct Swap
     static_assert(sizeof...(xs) == 2, "xs's length should be 2.");
 };
 
-template <typename... xs>
-using SwapT = typename Swap<xs...>::type;
-
 template <typename x, typename y>
 struct Swap<x, y> : type_is<TypeList<y, x>>
 { };
 
 
-template <typename x, typename y>
-struct Swap<TypeList<x, y>> : Swap<x, y>
-{ };
+template <typename... xs>
+using SwapT = typename Swap<xs...>::type;
 
-template <typename x, typename y>
-struct Swap<std::pair<x, y>> : type_is<std::pair<y, x>>
-{ };
 
-template <typename x, typename y>
-struct Swap<std::tuple<x, y>> : ToTuple<SwapT<x, y>>
+template <template <typename...> class TypeListContainer, typename x, typename y>
+struct Swap<TypeListContainer<x, y>>
+        : ReplaceTypeListContainer<SwapT<x, y>, TypeListContainer>
 { };
 
 
