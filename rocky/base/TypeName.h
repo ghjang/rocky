90f92f825ff5614d70e65e43a420760851355a50
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-05 01:28:47 +0900

    added template_name_of variable template and test.

--- rocky/base/TypeName.h
+++ rocky/base/TypeName.h
@@ -2,7 +2,10 @@
 #define ROCKY_BASE_TYPENAME_H
 
 
+#include <regex>
 #include <boost/type_index.hpp>
+
+#include "rocky/base/TypeListUtility.h"
 
 
 /**
@@ -15,7 +18,7 @@ struct TD;
 namespace Detail
 {
     template <typename T>
-    auto getTypeNameOf()
+    std::string getTypeNameOf()
     {
         std::string name = boost::typeindex::type_id_with_cvr<T>().pretty_name();
         auto pos = name.find_last_of(":");
@@ -23,6 +26,18 @@ namespace Detail
             return name.substr(pos + 1);
         }
         return name;
+    }
+
+    template <typename T>
+    std::string getTemplateNameOf()
+    {
+        std::regex expr{ R"(.*?([a-zA-Z0-9_]+)<.*>)" };
+        std::string name = boost::typeindex::type_id_with_cvr<T>().pretty_name();
+        std::smatch m;
+        if (std::regex_match(name, m, expr)) {
+            return m[1].str().c_str();
+        }
+        return "";
     }
 } // namespace Detail
 
@@ -32,6 +47,15 @@ std::string type_name_of{ Detail::getTypeNameOf<T>() };
 
 template <typename T>
 std::string type_full_name_of{ boost::typeindex::type_id_with_cvr<T>().pretty_name() };
+
+
+template <typename T, typename = void>
+std::string template_name_of{};
+
+template <typename T>
+std::string template_name_of<T, std::enable_if_t<IsTypeListContainer<std::decay_t<T>>::value>> {
+        Detail::getTemplateNameOf<std::decay_t<T>>()
+};
 
 
 #endif //ROCKY_BASE_TYPENAME_H
--- rocky/test/base/TypeNameTest.cpp
+++ rocky/test/base/TypeNameTest.cpp
@@ -3,6 +3,7 @@
 #include "rocky/base/TypeName.h"
 
 #include <iostream>
+#include <vector>
 
 
 TEST_CASE("type_name_of with local class", "[TypeName]")
@@ -19,5 +20,24 @@ TEST_CASE("type_name_of with local class", "[TypeName]")
 
     S const volatile * s2{};
     REQUIRE("S const volatile*" == type_name_of<decltype(s2)>);
+}
+
+TEST_CASE("template_name_of", "[TypeName]")
+{
+    using std::tuple;
+    using std::vector;
+
+    using vector_t = vector<char>;
+    using vector_c_t = vector<char> const;
+    using vector_cv_t = vector<char> const volatile;
+    using vector_p_t = vector<char> *;
+
+    REQUIRE("vector" == template_name_of<vector_t>);
+    REQUIRE("vector" == template_name_of<vector_c_t>);
+    REQUIRE("vector" == template_name_of<vector_cv_t>);
+    REQUIRE("" == template_name_of<vector_p_t>);
+
+    using tuple_t = tuple<char, int, float, double>;
+    REQUIRE("tuple" == template_name_of<tuple_t>);
 }
 
