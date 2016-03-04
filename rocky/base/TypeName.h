c6972112246dbf68fcc37da22be462527fa936a8
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-03-05 00:19:45 +0900

    added variable template type_name_of and test.

--- rocky/base/TypeName.h
+++ rocky/base/TypeName.h
@@ -2,11 +2,36 @@
 #define ROCKY_BASE_TYPENAME_H
 
 
+#include <boost/type_index.hpp>
+
+
 /**
  * For type displaying.
  */
 template <typename T>
 struct TD;
+
+
+namespace Detail
+{
+    template <typename T>
+    auto getTypeNameOf()
+    {
+        std::string name = boost::typeindex::type_id_with_cvr<T>().pretty_name();
+        auto pos = name.find_last_of(":");
+        if (pos != std::string::npos) {
+            return name.substr(pos + 1);
+        }
+        return name;
+    }
+} // namespace Detail
+
+
+template <typename T>
+std::string type_name_of{ Detail::getTypeNameOf<T>() };
+
+template <typename T>
+std::string type_full_name_of{ boost::typeindex::type_id_with_cvr<T>().pretty_name() };
 
 
 #endif //ROCKY_BASE_TYPENAME_H
--- rocky/test/base/TypeNameTest.cpp
+++ rocky/test/base/TypeNameTest.cpp
@@ -2,9 +2,22 @@
 
 #include "rocky/base/TypeName.h"
 
+#include <iostream>
 
-TEST_CASE("type_name_of", "[TypeName]")
+
+TEST_CASE("type_name_of with local class", "[TypeName]")
 {
-    
+    struct S { };
+
+    REQUIRE("S" == type_name_of<S>);
+
+    S const s{};
+    REQUIRE("S const" == type_name_of<decltype(s)>);
+
+    S const volatile s1{};
+    REQUIRE("S const volatile" == type_name_of<decltype(s1)>);
+
+    S const volatile * s2{};
+    REQUIRE("S const volatile*" == type_name_of<decltype(s2)>);
 }
 
