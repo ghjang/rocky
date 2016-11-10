781b38e2cdcde57157d609dc568ac42133dec89d
Gil Ho, Jang <jmxexpert@hotmail.com>
2016-11-10 12:15:09 +0900

    added some files for expression templates.

---
+++ rocky/et/ExpressionTemplate.h
@@ -0,0 +1,6 @@
+#ifndef ROCKY_ET_H
+#define ROCKY_ET_H
+
+
+
+#endif // ROCKY_ET_H
---
+++ rocky/test/et/ExpressionTemplateTest.cpp
@@ -0,0 +1,9 @@
+#include "../catch.hpp"
+
+#include "rocky/et/ExpressionTemplate.h"
+
+
+TEST_CASE("et", "[et]")
+{
+
+}
