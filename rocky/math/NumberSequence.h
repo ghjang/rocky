f753f28655aa99859f613143ce15cb499bc7c7cf
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-16 13:32:17 +0900

    modified number_seq function interface: - now it supports a way of 'init, increment, max' domain value generation control.

--- rocky/math/NumberSequence.h
+++ rocky/math/NumberSequence.h
@@ -3,36 +3,61 @@
 
 
 #include <type_traits>
+#include <limits>
 
 
-template <typename F>
+template <typename F, typename T>
 class number_seq_generator
 {
 public:
-    number_seq_generator(F const& f)
-        : x_{ 0 }
-        , f_{ f }
+    number_seq_generator(F const& f, T init, T increment, T max)
+        : f_{ f }
+        , x_{ init }
+        , increment_{ increment }
+        , max_{ max }
     { }
 
-    number_seq_generator(F && f)
-        : x_{ 0 }
-        , f_{ std::move(f) }
+    number_seq_generator(F && f, T init, T increment, T max)
+        : f_{ std::move(f) }
+        , x_{ init }
+        , increment_{ increment }
+        , max_{ max }
     { }
 
 public:
-    decltype(auto) operator () ()
-    { return f_(x_++); }
+    auto operator () ()
+    {
+        if (x_ > max_) {
+            // TODO: need to throw an exception
+        }
+        
+        auto tmp{ f_(x_) };
+
+        // TODO: need to check overflow.
+        x_ += increment_;
+        
+        return tmp;
+    }
 
 private:
-    int x_;
     F f_;
+    T x_;
+    T const increment_;
+    T const max_;
 };
 
 
-template <typename F>
-auto number_seq(F && f)
+template <typename F, typename T = int, typename U = T, typename V = T>
+auto number_seq(F && f, T init = 0, U increment = 1, V max = std::numeric_limits<V>::max())
 {
-    return number_seq_generator<std::decay_t<F>>{ std::forward<F>(f) };
+    assert(init <= max);
+    if (init > max) {
+        // TODO: throw an exception
+    }
+    return number_seq_generator<
+                std::decay_t<F>,
+                std::common_type_t<T, U, V>
+           >{ std::forward<F>(f), init, increment, max };
 }
 
 #endif // ROCKY_MATH_NUMBER_SEQUENCE_H
--- rocky/test/et/NumberSequenceTest.cpp
+++ rocky/test/et/NumberSequenceTest.cpp
@@ -11,9 +11,18 @@
 TEST_CASE("number sequence with range and et", "[et]")
 {
     using namespace ranges;
+
     auto generator = number_seq(2 * _1 + 1);
     std::vector<int> seq = view::generate(generator) | view::take(3);
     REQUIRE(ranges::equal(seq, { 1, 3, 5 }));
+
+    auto generator1 = number_seq(2 * _1 + 1, 10);
+    std::vector<int> seq1 = view::generate(generator1) | view::take(3);
+    REQUIRE(ranges::equal(seq1, { 21, 23, 25 }));
+
+    auto generator2 = number_seq(2 * _1 + 1, 10, 10);
+    std::vector<int> seq2 = view::generate(generator2) | view::take(3);
+    REQUIRE(ranges::equal(seq2, { 21, 41, 61 }));
 }
 
 TEST_CASE("number sequence with range and et - 1", "[et]")
