d221114d76186a2e014b8d44b823b420c1b353d0
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-20 23:11:12 +0900

    added files for the calculator. not implemented yet.

---
+++ rocky/math/calc/Calculator.h
@@ -0,0 +1,8 @@
+#ifndef ROCKY_MATH_CALC_CALCULATOR_H
+#define ROCKY_MATH_CALC_CALCULATOR_H
+
+
+#include <boost/spirit/include/qi.hpp>
+
+
+#endif // ROCKY_MATH_CALC_CALCULATOR_H
---
+++ rocky/test/math/calc/CalculatorTest.cpp
@@ -0,0 +1,9 @@
+#include "../catch.hpp"
+
+#include "rocky/math/Calculator.h"
+
+
+TEST_CASE("calculator", "[math]")
+{
+
+}
