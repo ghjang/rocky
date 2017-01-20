6987b8eb0e219036ac49f8f3ac10a803178cc0b6
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-01-20 23:55:28 +0900

    added basic calculator grammar. added a match test case.

--- rocky/math/calc/Calculator.h
+++ rocky/math/calc/Calculator.h
@@ -5,4 +5,38 @@
 #include <boost/spirit/include/qi.hpp>
 
 
+namespace rocky::math::calc
+{
+    namespace qi = boost::spirit::qi;
+
+    template <typename Iterator>
+    struct calculator : qi::grammar<Iterator, qi::ascii::space_type>
+    {
+        calculator() : calculator::base_type(expr_)
+        {
+            using qi::uint_;
+
+            expr_ = term_ >> *(
+                                '+' >> term_
+                              | '-' >> term_
+                              );
+
+            term_ = factor_ >> *(
+                                    '*' >> factor_
+                                |   '/' >> factor_
+                                );
+
+            factor_ = uint_
+                        | '(' >> expr_ >> ')'
+                        | ('-' >> factor_)
+                        | ('+' >> factor_);
+        }
+
+        qi::rule<Iterator, qi::ascii::space_type> expr_;
+        qi::rule<Iterator, qi::ascii::space_type> term_;
+        qi::rule<Iterator, qi::ascii::space_type> factor_;
+    };
+} // namespace rocky::math::calc
+
+
 #endif // ROCKY_MATH_CALC_CALCULATOR_H
--- rocky/test/math/calc/CalculatorTest.cpp
+++ rocky/test/math/calc/CalculatorTest.cpp
@@ -1,9 +1,20 @@
-#include "../catch.hpp"
+#include "../../catch.hpp"
 
-#include "rocky/math/Calculator.h"
+#include "rocky/math/calc/Calculator.h"
 
 
 TEST_CASE("calculator", "[math]")
 {
+    namespace qi = boost::spirit::qi;
+    using rocky::math::calc::calculator;
 
+    std::string exprStr = "10 * (-20 + 30)";
+    
+    calculator<std::string::iterator> calc_;
+
+    auto begin = exprStr.begin();
+    auto end = exprStr.end();
+    bool r = qi::phrase_parse(begin, end, calc_, qi::ascii::space);
+    REQUIRE(r);
+    REQUIRE(begin == end);
 }
